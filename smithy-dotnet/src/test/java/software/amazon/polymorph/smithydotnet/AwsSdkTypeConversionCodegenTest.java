af1b50ec65a59f868e4454e8aa3dc1b0b7dba59a
Alex Chew <alexchew@amazon.com>
2022-05-12 16:52:06 -0700

    add test

--- smithy-dotnet/src/test/java/software/amazon/polymorph/smithydotnet/AwsSdkTypeConversionCodegenTest.java
+++ smithy-dotnet/src/test/java/software/amazon/polymorph/smithydotnet/AwsSdkTypeConversionCodegenTest.java
@@ -33,6 +33,50 @@ public class AwsSdkTypeConversionCodegenTest {
         return new AwsSdkTypeConversionCodegen(model, SERVICE_SHAPE_ID);
     }
 
+    /**
+     * Test that an AWS SDK structure type converter doesn't try to call the member properties' {@code IsSet*} methods,
+     * since those are marked internal and are thus inaccessible to our generated code.
+     */
+    @Test
+    public void testGenerateStructureConverterWithOptionalValue() {
+        final ShapeId shapeId = ShapeId.fromParts(SERVICE_NAMESPACE, "OptionalInt");
+        final ShapeId memberId = shapeId.withMember("int");
+        final TypeConversionCodegen codegen = setupCodegen((builder, modelAssembler) ->
+                modelAssembler.addUnparsedModel("test.smithy", """
+                        namespace %s
+                        structure OptionalInt { int: Integer }
+                        """.formatted(SERVICE_NAMESPACE)));
+        final TypeConversionCodegen.TypeConverter converter = codegen.generateStructureConverter(
+                codegen.getModel().expectShape(shapeId, StructureShape.class));
+        assertEquals(shapeId, converter.shapeId());
+
+        final List<Tokenizer.ParseToken> actualTokensFromDafny = Tokenizer.tokenize(converter.fromDafny().toString());
+        final String structureFromDafnyConverterName = AwsSdkDotNetNameResolver.typeConverterForShape(shapeId, FROM_DAFNY);
+        final String memberFromDafnyConverterName = AwsSdkDotNetNameResolver.typeConverterForShape(memberId, FROM_DAFNY);
+        final List<Tokenizer.ParseToken> expectedTokensFromDafny = Tokenizer.tokenize("""
+                public static Amazon.FoobarService.Model.OptionalInt
+                        %s(Dafny.Com.Amazonaws.Foobar._IOptionalInt value) {
+                    Dafny.Com.Amazonaws.Foobar.OptionalInt concrete = (Dafny.Com.Amazonaws.Foobar.OptionalInt)value;
+                    Amazon.FoobarService.Model.OptionalInt converted = new Amazon.FoobarService.Model.OptionalInt();
+                    if (concrete.int.is_Some) converted.Int = (int) %s(concrete.int);
+                    return converted;
+                }
+                """.formatted(structureFromDafnyConverterName, memberFromDafnyConverterName));
+        assertEquals(expectedTokensFromDafny, actualTokensFromDafny);
+
+        final List<Tokenizer.ParseToken> actualTokensToDafny = Tokenizer.tokenize(converter.toDafny().toString());
+        final String structureToDafnyConverterName = AwsSdkDotNetNameResolver.typeConverterForShape(shapeId, TO_DAFNY);
+        final String memberToDafnyConverterName = AwsSdkDotNetNameResolver.typeConverterForShape(memberId, TO_DAFNY);
+        final List<Tokenizer.ParseToken> expectedTokensToDafny = Tokenizer.tokenize("""
+                public static Dafny.Com.Amazonaws.Foobar._IOptionalInt
+                        %s(Amazon.FoobarService.Model.OptionalInt value) {
+                    int? var_int = value.Int;
+                    return new Dafny.Com.Amazonaws.Foobar.OptionalInt(%s(var_int));
+                }
+                """.formatted(structureToDafnyConverterName, memberToDafnyConverterName));
+        assertEquals(expectedTokensToDafny, actualTokensToDafny);
+    }
+
     @Test
     public void testGenerateStructureConverterErrorStructureWithMessage() {
         final ShapeId shapeId = ShapeId.fromParts(SERVICE_NAMESPACE, "OopsException");
