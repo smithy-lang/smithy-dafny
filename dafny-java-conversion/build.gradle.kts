bc0a0c5dd4737637fa9ba39070d67d3a68d35447
texastony <5892063+texastony@users.noreply.github.com>
2022-09-30 16:26:15 -0700

    fix: gradle issues

---
+++ dafny-java-conversion/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+group = "software.amazon.dafny"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    implementation("dafny.lang:DafnyRuntime:3.8.1")
+    testImplementation("junit", "junit", "4.13.2")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("conversion") {
+            from(components["java"])
+        }
+    }
+    repositories{ mavenLocal() }
+}
