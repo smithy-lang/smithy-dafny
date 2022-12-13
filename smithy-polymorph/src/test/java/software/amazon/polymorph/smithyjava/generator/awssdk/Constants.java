e4bdf5afe6f0d88ed400ffa937d4ce0e5e223956
Lucas McDonald <lucmcdon@amazon.com>
2022-12-13 11:25:36 -0800

    refactor: Fix indentation

--- smithy-polymorph/src/test/java/software/amazon/polymorph/smithyjava/generator/awssdk/Constants.java
+++ smithy-polymorph/src/test/java/software/amazon/polymorph/smithyjava/generator/awssdk/Constants.java
@@ -34,24 +34,24 @@ public class Constants {
 
     static String MockKmsShim = """
             package Dafny.Com.Amazonaws.Kms;
-                        
+              
             import Dafny.Com.Amazonaws.Kms.Types.DoSomethingRequest;
             import Dafny.Com.Amazonaws.Kms.Types.DoSomethingResponse;
             import Dafny.Com.Amazonaws.Kms.Types.DoVoidRequest;
             import Dafny.Com.Amazonaws.Kms.Types.Error;
             import Dafny.Com.Amazonaws.Kms.Types.IKeyManagementServiceClient;
-                        
+            
             import Wrappers_Compile.Result;
-                        
+            
             import com.amazonaws.services.kms.AWSKMS;
             import com.amazonaws.services.kms.model.AWSKMSException;
             import com.amazonaws.services.kms.model.DependencyTimeoutException;
             import com.amazonaws.services.kms.model.DoSomethingResult;
             import dafny.Tuple0;
-                        
+            
             import java.lang.Override;
             import java.lang.String;
-                        
+            
             public class Shim implements IKeyManagementServiceClient {
               private final AWSKMS _impl;
               
