27ad2fe4cab5d55dc2bb8debd5962524ce79742d
Lucas McDonald <lucmcdon@amazon.com>
2022-12-13 11:24:48 -0800

    refactor: Fix indentation

--- smithy-polymorph/src/test/java/software/amazon/polymorph/smithyjava/generator/awssdk/Constants.java
+++ smithy-polymorph/src/test/java/software/amazon/polymorph/smithyjava/generator/awssdk/Constants.java
@@ -33,42 +33,42 @@ public class Constants {
             """;
 
     static String MockKmsShim = """
-        package Dafny.Com.Amazonaws.Kms;
-                    
-        import Dafny.Com.Amazonaws.Kms.Types.DoSomethingRequest;
-        import Dafny.Com.Amazonaws.Kms.Types.DoSomethingResponse;
-        import Dafny.Com.Amazonaws.Kms.Types.DoVoidRequest;
-        import Dafny.Com.Amazonaws.Kms.Types.Error;
-        import Dafny.Com.Amazonaws.Kms.Types.IKeyManagementServiceClient;
-                    
-        import Wrappers_Compile.Result;
-                    
-        import com.amazonaws.services.kms.AWSKMS;
-        import com.amazonaws.services.kms.model.AWSKMSException;
-        import com.amazonaws.services.kms.model.DependencyTimeoutException;
-        import com.amazonaws.services.kms.model.DoSomethingResult;
-        import dafny.Tuple0;
-                    
-        import java.lang.Override;
-        import java.lang.String;
-                    
-        public class Shim implements IKeyManagementServiceClient {
-          private final AWSKMS _impl;
-          
-          private final String region;
-          
-          public Shim(final AWSKMS impl, final String region) {
-            this._impl = impl;
-            this.region = region;
-          }
-          
-          public String region() {
-            return this.region;
-          }
-          
-          %s
-          %s
-        }
-        """.formatted(DoSomethingOperation, DoVoidOperation);
+            package Dafny.Com.Amazonaws.Kms;
+                        
+            import Dafny.Com.Amazonaws.Kms.Types.DoSomethingRequest;
+            import Dafny.Com.Amazonaws.Kms.Types.DoSomethingResponse;
+            import Dafny.Com.Amazonaws.Kms.Types.DoVoidRequest;
+            import Dafny.Com.Amazonaws.Kms.Types.Error;
+            import Dafny.Com.Amazonaws.Kms.Types.IKeyManagementServiceClient;
+                        
+            import Wrappers_Compile.Result;
+                        
+            import com.amazonaws.services.kms.AWSKMS;
+            import com.amazonaws.services.kms.model.AWSKMSException;
+            import com.amazonaws.services.kms.model.DependencyTimeoutException;
+            import com.amazonaws.services.kms.model.DoSomethingResult;
+            import dafny.Tuple0;
+                        
+            import java.lang.Override;
+            import java.lang.String;
+                        
+            public class Shim implements IKeyManagementServiceClient {
+              private final AWSKMS _impl;
+              
+              private final String region;
+              
+              public Shim(final AWSKMS impl, final String region) {
+                this._impl = impl;
+                this.region = region;
+              }
+              
+              public String region() {
+                return this.region;
+              }
+              
+              %s
+              %s
+            }
+            """.formatted(DoSomethingOperation, DoVoidOperation);
 
 }
