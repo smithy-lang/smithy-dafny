b1660dd467f245ce77e6566536ac798fcd8c996a
texastony <5892063+texastony@users.noreply.github.com>
2023-02-06 12:37:49 -0800

    fix(smithy-polymorph-tests): SimpleResources w/o NativeWrapper

--- TestModels/resources/runtimes/net/Generated/Wrapped/SimpleResourcesShim.cs
+++ TestModels/resources/runtimes/net/Generated/Wrapped/SimpleResourcesShim.cs
@@ -1,43 +1,32 @@
 // Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 // Do not modify this file. This file is machine generated, and any changes to it will be overwritten.
-
 using System;
 using System.IO;
 using System.Collections.Generic;
-
 namespace Simple.Resources.Wrapped
 {
     public class SimpleResourcesShim : Dafny.Simple.Resources.Types.ISimpleResourcesClient
     {
         public Simple.Resources.SimpleResources _impl;
-
         public SimpleResourcesShim(Simple.Resources.SimpleResources impl)
         {
             this._impl = impl;
         }
-
-        public Wrappers_Compile._IResult<Dafny.Simple.Resources.Types._IGetResourcesOutput,
-          Dafny.Simple.Resources.Types._IError> GetResources(Dafny.Simple.Resources.Types._IGetResourcesInput request)
+        public Wrappers_Compile._IResult<Dafny.Simple.Resources.Types._IGetResourcesOutput, Dafny.Simple.Resources.Types._IError> GetResources(Dafny.Simple.Resources.Types._IGetResourcesInput request)
         {
-            Simple.Resources.GetResourcesInput unWrappedRequest =
-              TypeConversion.FromDafny_N6_simple__N9_resources__S17_GetResourcesInput(request);
-            try
+            Simple.Resources.GetResourcesInput unWrappedRequest = TypeConversion.FromDafny_N6_simple__N9_resources__S17_GetResourcesInput(request); try
             {
                 Simple.Resources.GetResourcesOutput wrappedResponse =
-                  this._impl.GetResources(unWrappedRequest);
-                return Wrappers_Compile
-                  .Result<Dafny.Simple.Resources.Types._IGetResourcesOutput, Dafny.Simple.Resources.Types._IError>
-                  .create_Success(TypeConversion.ToDafny_N6_simple__N9_resources__S18_GetResourcesOutput(wrappedResponse));
+                this._impl.GetResources(unWrappedRequest);
+                return Wrappers_Compile.Result<Dafny.Simple.Resources.Types._IGetResourcesOutput, Dafny.Simple.Resources.Types._IError>.create_Success(TypeConversion.ToDafny_N6_simple__N9_resources__S18_GetResourcesOutput(wrappedResponse));
             }
             catch (System.Exception ex)
             {
-                return Wrappers_Compile
-                  .Result<Dafny.Simple.Resources.Types._IGetResourcesOutput, Dafny.Simple.Resources.Types._IError>
-                  .create_Failure(this.ConvertError(ex));
+                return Wrappers_Compile.Result<Dafny.Simple.Resources.Types._IGetResourcesOutput, Dafny.Simple.Resources.Types._IError>.create_Failure(this.ConvertError(ex));
             }
-        }
 
+        }
         private Dafny.Simple.Resources.Types._IError ConvertError(System.Exception error)
         {
             switch (error)
@@ -47,6 +36,7 @@ namespace Simple.Resources.Wrapped
 
                 default:
                     return new Dafny.Simple.Resources.Types.Error_Opaque(error);
+
             }
         }
     }
