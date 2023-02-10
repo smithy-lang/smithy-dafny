smithy-lang/smithy-dafny

commit 0d1c8b268a274b6218da3ed8f8316aefbfc12dd9

refactor(TestModels): Extendable
Apply PR #137 suggestions

Co-authored-by: seebees <ryanemer@amazon.com>

TestModels/Extendable/src/SimpleExtendableResourcesOperations.dfy
@@ -56,15 +56,11 @@ module SimpleExtendableResourcesOperations refines AbstractSimpleExtendableResou
   )
   {
     var resource := input.resource;
-    var maybeData := resource.GetResourceData(input.input);
-    if (maybeData.Success?) {
-      var result := UseExtendableResourceOutput(
-        output := maybeData.Extract()
-      );
-      return Success(result);
-    } else {
-      return maybeData.PropagateFailure<UseExtendableResourceOutput>();
-    }
+    var output :- resource.GetResourceData(input.input);
+    var result := UseExtendableResourceOutput(
+      output := output
+    );
+    return Success(result);
   }
 
   predicate UseExtendableResourceAlwaysModeledErrorEnsuresPublicly(

TestModels/Extendable/test/Helpers.dfy
@@ -11,11 +11,11 @@ module TestHelpers {
   function method allNone(): Types.GetResourceDataInput
   {
    Types.GetResourceDataInput(
-      blobValue := Option.None(),
-      booleanValue := Option.None(),
-      stringValue := Option.None(),
-      integerValue := Option.None(),
-      longValue := Option.None()
+      blobValue := None,
+      booleanValue := None,
+      stringValue := None,
+      integerValue := None,
+      longValue := None
     )
   }
 
@@ -23,11 +23,11 @@ module TestHelpers {
     output: Types.GetResourceDataOutput
   )
   {
-    expect Option.None == output.stringValue;
-    expect Option.None() == output.blobValue;
-    expect Option.None() == output.booleanValue;
-    expect Option.None() == output.integerValue;
-    expect Option.None() == output.longValue; 
+    expect output.stringValue.None?;
+    expect output.blobValue.None?;
+    expect output.booleanValue.None?;
+    expect output.integerValue.None?;
+    expect output.longValue;.None?
   }  
 
   function method allSome(): Types.GetResourceDataInput
@@ -57,7 +57,7 @@ module TestHelpers {
   )
   {
     expect errorOutput.Failure?;
-    var actualError := errorOutput.PropagateFailure<Types.GetExtendableResourceErrorsOutput>().error;
+    var actualError := errorOutput.error;
     expect actualError == Types.SimpleExtendableResourcesException(
       message := "Hard Coded Exception in src/dafny"
     );
@@ -91,16 +91,6 @@ module TestHelpers {
   {
     expect errorOutput.Failure?;
     var actualError := errorOutput.PropagateFailure<Types.GetExtendableResourceErrorsOutput>().error;
-    // TypeConversion.FromDafny_CommonError does not handle Collection.
-    // As such, even though AlwaysMutlipleErrors correctly returns a Collection in Dafny,
-    // the generated .NET code botches the conversion,
-    // and lumps it as an Opaue Exception.
-    // TODO:
-    // Once https://github.com/awslabs/polymorph/pull/136 is resolved/merged
-    // and smithy-dotnet handles Collections, replace
-    // expect actualError.Opaque? with commented out expect,
-    // and remove this comment.
-    // expect actualError.Opaque?;
     expect actualError.Collection?
       && |actualError.list| == 1
       && actualError.list[0].SimpleExtendableResourcesException?

TestModels/Extendable/src/ExtendableResource.dfy
@@ -9,6 +9,7 @@ module ExtendableResource {
   import Types = SimpleExtendableResourcesTypes
 
   class OpaqueMessage {
+    // See the comments in `CheckOpaqueError` of `../test/Helpers.dfy` for more details
     const message: string := "Hard Coded Opaque Message that will not survive translation.";
     constructor () {}
   }

commit 360f28d3df5d6f79a5a31ac2b8e4187e6dc9b2d6

fix(TestModels): Extendable Dafny source
Fixes the Dafny Syntax errors introduced in 0d1c8b2

TestModels/Extendable/src/SimpleExtendableResourcesOperations.dfy
@@ -56,9 +56,9 @@ module SimpleExtendableResourcesOperations refines AbstractSimpleExtendableResou
   )
   {
     var resource := input.resource;
-    var output :- resource.GetResourceData(input.input);
-    var result := UseExtendableResourceOutput(
-      output := output
+    var data :- resource.GetResourceData(input.input);
+    var result := Types.UseExtendableResourceOutput(
+      output := data
     );
     return Success(result);
   }

TestModels/Extendable/test/Helpers.dfy
@@ -27,7 +27,7 @@ module TestHelpers {
     expect output.blobValue.None?;
     expect output.booleanValue.None?;
     expect output.integerValue.None?;
-    expect output.longValue;.None?
+    expect output.longValue.None?;
   }  
 
   function method allSome(): Types.GetResourceDataInput