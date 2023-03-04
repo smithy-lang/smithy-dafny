91585123daa195f04ffef5da4a8ec1cc5d6dded0
Robin Salkeld <salkeldr@amazon.com>
2023-03-03 16:20:56 -0800

    Just smithy model validation working

---
+++ TestModels/aws-sdks/sqs/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("software.amazon.smithy").version("0.6.0")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("software.amazon.smithy:smithy-model:1.28.0")
+    implementation("software.amazon.smithy:smithy-aws-traits:1.28.0")
+    implementation("software.amazon.smithy:smithy-rules-engine:1.28.0")
+}
+
+configure<software.amazon.smithy.gradle.SmithyExtension> {
+    // Uncomment this to use a custom projection when building the JAR.
+    // projection = "foo"
+}
+
+// Uncomment to disable creating a JAR.
+//tasks["jar"].enabled = false
