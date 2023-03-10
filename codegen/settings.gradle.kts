cf87406774d42b88000a33316a310293ab5fd6af
Alex Chew <alexchew@amazon.com>
2023-03-10 12:55:43 -0800

    add license headers

--- codegen/settings.gradle.kts
+++ codegen/settings.gradle.kts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 rootProject.name = "smithy-dafny"
 include(":smithy-dafny-codegen")
 include(":smithy-dafny-codegen-test")
--- codegen/smithy-dafny-codegen/build.gradle.kts
+++ codegen/smithy-dafny-codegen/build.gradle.kts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 description = "Generates Dafny code from Smithy models"
 extra["displayName"] = "Smithy :: Dafny :: Codegen"
 extra["moduleName"] = "software.amazon.smithy.dafny.codegen"
