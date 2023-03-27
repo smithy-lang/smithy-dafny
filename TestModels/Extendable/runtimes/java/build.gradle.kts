54bced975f53a560f3ea7d32a741a36939601c64
texastony <5892063+texastony@users.noreply.github.com>
2023-03-27 13:09:29 -0700

    test(Extendable): Java

---
+++ TestModels/Extendable/runtimes/java/build.gradle.kts
@@ -0,0 +1,56 @@
+tasks.wrapper {
+    gradleVersion = "7.6"
+}
+
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "simple.extendable.resources"
+version = "1.0-SNAPSHOT"
+description = "SimpleExtendableResources"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+    sourceSets["main"].java {
+        srcDir("src/main/java")
+        srcDir("src/main/dafny-generated")
+        srcDir("src/main/smithy-generated")
+    }
+    sourceSets["test"].java {
+        srcDir("src/test/java")
+        srcDir("src/test/dafny-generated")
+    }
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    implementation("dafny.lang:DafnyRuntime:3.10.0")
+    implementation("software.amazon.dafny:conversion:1.0-SNAPSHOT")
+    implementation("software.amazon.cryptography:StandardLibrary:1.0-SNAPSHOT")
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        groupId = "simple"
+        artifactId = "Constraints"
+        from(components["java"])
+    }
+    repositories { mavenLocal() }
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+tasks {
+    register("runTests", JavaExec::class.java) {
+        mainClass.set("TestsFromDafny")
+        classpath = sourceSets["test"].runtimeClasspath
+    }
+}
