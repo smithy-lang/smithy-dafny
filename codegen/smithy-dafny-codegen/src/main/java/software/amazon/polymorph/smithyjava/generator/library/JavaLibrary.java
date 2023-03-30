a105c765009aee0b0c3b6909e93e5c01db8f5c91
Alex Chew <alex-chew@users.noreply.github.com>
2023-03-30 11:14:14 -0700

    Revert "Revert "chore: move codegen logic (#206)" (#208)" (#209)

---
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithyjava/generator/library/JavaLibrary.java
@@ -0,0 +1,232 @@
+package software.amazon.polymorph.smithyjava.generator.library;
+
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.CodeBlock;
+import com.squareup.javapoet.MethodSpec;
+
+import java.nio.file.Path;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import software.amazon.polymorph.smithyjava.NamespaceHelper;
+import software.amazon.polymorph.smithyjava.generator.CodegenSubject;
+import software.amazon.polymorph.smithyjava.generator.Generator;
+import software.amazon.polymorph.smithyjava.generator.awssdk.v1.ShimV1;
+import software.amazon.polymorph.smithyjava.generator.awssdk.v2.ShimV2;
+import software.amazon.polymorph.smithyjava.generator.library.shims.ResourceShim;
+import software.amazon.polymorph.smithyjava.generator.library.shims.ServiceShim;
+import software.amazon.polymorph.traits.LocalServiceTrait;
+import software.amazon.polymorph.traits.PositionalTrait;
+import software.amazon.polymorph.traits.ReferenceTrait;
+import software.amazon.polymorph.utils.DafnyNameResolverHelpers;
+import software.amazon.polymorph.utils.ModelUtils;
+import software.amazon.polymorph.utils.TokenTree;
+import software.amazon.polymorph.smithyjava.nameresolver.Dafny;
+import software.amazon.polymorph.smithyjava.nameresolver.Native;
+
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.node.ExpectationNotMetException;
+import software.amazon.smithy.model.shapes.ListShape;
+import software.amazon.smithy.model.shapes.MapShape;
+import software.amazon.smithy.model.shapes.MemberShape;
+import software.amazon.smithy.model.shapes.ResourceShape;
+import software.amazon.smithy.model.shapes.ServiceShape;
+import software.amazon.smithy.model.shapes.SetShape;
+import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.model.shapes.StringShape;
+import software.amazon.smithy.model.shapes.StructureShape;
+import software.amazon.smithy.model.shapes.UnionShape;
+import software.amazon.smithy.model.traits.EnumTrait;
+import software.amazon.smithy.model.traits.ErrorTrait;
+import software.amazon.smithy.model.traits.TraitDefinition;
+
+public class JavaLibrary extends CodegenSubject {
+
+    /** Public Java Interfaces will go here. */
+    public final String packageName;
+    /** Public POJOs will go here. */
+    public final String modelPackageName;
+    protected final ToDafnyLibrary toDafnyLibrary;
+    protected final ToNativeLibrary toNativeLibrary;
+
+    public JavaLibrary(Model model, ServiceShape serviceShape, AwsSdkVersion sdkVersion) {
+        super(model, serviceShape, initDafny(model, serviceShape, sdkVersion), initNative(model, serviceShape, sdkVersion), sdkVersion);
+        packageName = NamespaceHelper.standardize(serviceShape.getId().getNamespace());
+        modelPackageName = packageName + ".model";
+        try {
+            serviceShape.expectTrait(LocalServiceTrait.class);
+        } catch (ExpectationNotMetException ex) {
+            throw new IllegalArgumentException(
+                    "JavaLibrary's MUST have a localService trait. ShapeId: %s".formatted(serviceShape.getId()),
+                    ex
+            );
+        }
+        toDafnyLibrary = new ToDafnyLibrary(this);
+        toNativeLibrary = new ToNativeLibrary(this);
+    }
+
+    static Dafny initDafny(Model model, ServiceShape serviceShape, AwsSdkVersion awsSdkVersion) {
+        String packageName = DafnyNameResolverHelpers.packageNameForNamespace(serviceShape.getId().getNamespace());
+        return new Dafny(packageName, model, serviceShape, awsSdkVersion);
+    }
+
+    static Native initNative(Model model, ServiceShape serviceShape, AwsSdkVersion awsSdkVersion) {
+        String packageName = NamespaceHelper.standardize(serviceShape.getId().getNamespace());
+        return new Native(packageName, serviceShape, model, packageName + ".model", awsSdkVersion);
+    }
+
+    public static CodeBlock wrapAwsService(
+            Shape shape, CodeBlock nativeValue, CodeBlock regionVar,
+            AwsSdkVersion sdkVersion) {
+        Optional<ServiceShape> serviceShape = shape.asServiceShape();
+        if (serviceShape.isEmpty()) {
+            throw new IllegalArgumentException("Shape must be Service");
+        }
+        return switch (sdkVersion) {
+            case V1 -> CodeBlock.of("new $T($L, $L)",
+                    ShimV1.className(serviceShape.get()),
+                    nativeValue,
+                    regionVar);
+            case V2 -> CodeBlock.of("new $T($L, $L)",
+                    ShimV2.className(serviceShape.get()),
+                    nativeValue,
+                    regionVar);
+        };
+    }
+
+    @SuppressWarnings("unused") // We do not use this yet (2023-03-05), but we might soon-ish. Remove by 2023-06 if still not used.
+    protected static CodeBlock castAndUnwrapAwsService(
+            Shape shape, CodeBlock dafnyValue,
+            AwsSdkVersion sdkVersion) {
+        Optional<ServiceShape> serviceShape = shape.asServiceShape();
+        if (serviceShape.isEmpty()) {
+            throw new IllegalArgumentException("Shape must be Service");
+        }
+        return switch (sdkVersion) {
+            case V1 -> CodeBlock.of("(($T) $L).impl()",
+                    ShimV1.className(serviceShape.get()),
+                    dafnyValue);
+            case V2 -> CodeBlock.of("(($T) $L).impl()",
+                    ShimV2.className(serviceShape.get()),
+                    dafnyValue);
+        };
+    }
+
+    /**
+     * @param method      MethodSpec.Builder that SHOULD have Parameters,
+     *                    Returns, & Modifiers set correctly
+     *                    ( note that
+     *                    void or parameterless methods would
+     *                    not have any Returns or Parameters).
+     * @param resolvedInput  A ResolvedShapeId representing the input
+     * @param resolvedOutput A ResolvedShapeId representing the output
+     */
+    public record MethodSignature(
+            MethodSpec.Builder method,
+            ModelUtils.ResolvedShapeId resolvedInput,
+            ModelUtils.ResolvedShapeId resolvedOutput
+    ) {}
+
+    @Override
+    public Map<Path, TokenTree> generate() {
+        Map<Path, TokenTree> rtn = new LinkedHashMap<>();
+        ModelCodegen serviceCodegen = new ModelCodegen(this);
+        rtn.putAll(serviceCodegen.generate());
+        rtn.putAll(toDafnyLibrary.generate());
+        rtn.putAll(toNativeLibrary.generate());
+        ShimLibrary shim = new ServiceShim(this, this.serviceShape);
+        rtn.putAll(shim.generate());
+        getResourcesInServiceNamespace().stream()
+                .map(shape -> new ResourceShim(this, shape))
+                .map(Generator::generate)
+                .forEachOrdered(rtn::putAll);
+        return rtn;
+    }
+
+    public List<StructureShape> getErrorsInServiceNamespace() {
+        return this.model.getStructureShapes().stream()
+                .filter(shape -> shape.hasTrait(ErrorTrait.class))
+                .filter(shape -> ModelUtils.isInServiceNamespace(shape.getId(), this.serviceShape))
+                .toList();
+    }
+
+    public List<StructureShape> getStructuresInServiceNamespace() {
+        return this.model.getStructureShapes().stream()
+                .filter(shape -> !shape.hasTrait(ErrorTrait.class))
+                .filter(shape -> !shape.hasTrait(TraitDefinition.class))
+                .filter(shape -> !shape.hasTrait(EnumTrait.class))
+                // We do not generate POJOs or To(Dafny,Native) for reference shapes
+                .filter(shape -> !shape.hasTrait(ReferenceTrait.class))
+                // We do not generate POJOs or To(Dafny,Native) for indirect reference shapes
+                .filter(shape -> {
+                    if (!shape.hasTrait(PositionalTrait.class)) {
+                        return true;
+                    }
+                    final MemberShape onlyMember = PositionalTrait.onlyMember(shape);
+                    final Shape targetShape = model.expectShape(onlyMember.getTarget());
+                    return !targetShape.hasTrait(ReferenceTrait.class);
+                })
+                .filter(shape -> ModelUtils.isInServiceNamespace(shape.getId(), this.serviceShape))
+                .toList();
+    }
+
+    public List<ResourceShape> getResourcesInServiceNamespace() {
+        return this.model.getResourceShapes().stream()
+                .filter(shape -> ModelUtils.isInServiceNamespace(shape.getId(), this.serviceShape))
+                .toList();
+    }
+
+    public List<StringShape> getEnumsInServiceNamespace() {
+        return this.model.getStringShapesWithTrait(EnumTrait.class).stream()
+                .filter(shape -> ModelUtils.isInServiceNamespace(shape.getId(), this.serviceShape))
+                .toList();
+    }
+
+    public List<UnionShape> getUnionsInServiceNamespace() {
+        return this.model.getUnionShapes().stream().sequential()
+                .filter(shape -> ModelUtils.isInServiceNamespace(shape.getId(), this.serviceShape))
+                .collect(Collectors.toList());
+    }
+
+    public List<ListShape> getListsInServiceNamespace() {
+        return this.model.getListShapes().stream().sequential()
+                .filter(shape -> ModelUtils.isInServiceNamespace(shape.getId(), this.serviceShape))
+                .collect(Collectors.toList());
+    }
+
+    public List<SetShape> getSetsInServiceNamespace() {
+        return this.model.getSetShapes().stream().sequential()
+                .filter(shape -> ModelUtils.isInServiceNamespace(shape.getId(), this.serviceShape))
+                .collect(Collectors.toList());
+    }
+
+    public List<MapShape> getMapsInServiceNamespace() {
+        return this.model.getMapShapes().stream().sequential()
+                .filter(shape -> ModelUtils.isInServiceNamespace(shape.getId(), this.serviceShape))
+                .collect(Collectors.toList());
+    }
+
+    public CodeBlock wrapWithShim(ShapeId referentId, CodeBlock referentVariable) throws ExpectationNotMetException {
+        final Shape targetShape = model.expectShape(referentId);
+        final ClassName rtnClassName;
+        if (targetShape.isResourceShape()) {
+            //noinspection OptionalGetWithoutIsPresent
+            ResourceShape rShape = targetShape.asResourceShape().get();
+            rtnClassName = nativeNameResolver.classNameForResource(rShape);
+            return CodeBlock.of("$T.$L($L)",
+                    rtnClassName, ResourceShim.WRAP_METHOD_NAME, referentVariable);
+        } else {
+            // It MUST be a service, as reference traits ONLY reference Resources & Services
+            //noinspection OptionalGetWithoutIsPresent
+            ServiceShape sShape = targetShape.asServiceShape().get();
+            rtnClassName = nativeNameResolver.classNameForService(sShape);
+        }
+        return CodeBlock.of("new $T($L)",
+                rtnClassName,
+                referentVariable);
+    }
+}
---
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithyjava/nameresolver/AwsSdkNativeV2.java
@@ -0,0 +1,363 @@
+package software.amazon.polymorph.smithyjava.nameresolver;
+
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.CodeBlock;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import software.amazon.polymorph.smithyjava.generator.CodegenSubject;
+import software.amazon.polymorph.smithyjava.generator.awssdk.v2.JavaAwsSdkV2;
+import software.amazon.polymorph.utils.AwsSdkNameResolverHelpers;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.shapes.MemberShape;
+import software.amazon.smithy.model.shapes.ServiceShape;
+import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.model.shapes.ShapeType;
+import software.amazon.smithy.model.shapes.StructureShape;
+import software.amazon.smithy.model.traits.EnumTrait;
+import software.amazon.smithy.model.traits.ErrorTrait;
+import software.amazon.smithy.model.traits.TraitDefinition;
+import software.amazon.smithy.utils.StringUtils;
+
+import software.amazon.awssdk.codegen.naming.DefaultNamingStrategy;
+import software.amazon.awssdk.codegen.model.service.ServiceModel;
+
+import static software.amazon.polymorph.smithyjava.nameresolver.AwsSdkV2NameResolverUtils.isAttributeValueType;
+import static software.amazon.polymorph.smithyjava.nameresolver.AwsSdkV2NameResolverUtils.tokenToUncapitalizeInShape;
+import static software.amazon.polymorph.smithyjava.nameresolver.Constants.SHAPE_TYPES_LIST_SET_MAP;
+import static software.amazon.smithy.utils.StringUtils.uncapitalize;
+
+/**
+ * There are certain assumptions we can/have to make about
+ * Types from the AWS SDK for Java V2 libraries.
+ */
+public class AwsSdkNativeV2 extends Native {
+
+    private final DefaultNamingStrategy awsSDKNaming;
+
+    public AwsSdkNativeV2(final ServiceShape serviceShape,
+                          final Model model) {
+        super(packageNameForAwsSdkV2Shape(serviceShape),
+                serviceShape,
+                model,
+                defaultModelPackageName(packageNameForAwsSdkV2Shape(serviceShape)),
+                CodegenSubject.AwsSdkVersion.V2);
+        checkForAwsServiceConstants();
+        awsSDKNaming = new DefaultNamingStrategy(new ServiceModel(), null);
+    }
+
+    // The values of these maps are NOT in smithy models and thus must be hard-coded
+    private static final Map<String, String> AWS_SERVICE_NAMESPACE_TO_CLIENT_INTERFACE;
+    private static final Map<String, String> AWS_SERVICE_NAMESPACE_TO_BASE_EXCEPTION;
+
+    static {
+        // The namespaces used as keys in these maps correspond to the Smithy namespace,
+        // NOT the SDK V2 namespace.
+        // Smithy namespace: com.amazonaws.X
+        // AWSSDK V2 namespace: software.amazon.awssdk.X
+
+        // These clients are NOT located in the services' model package;
+        // they are located in its parent namespace.
+        // i.e: kms : software.amazon.awssdk.kms.KmsClient
+        AWS_SERVICE_NAMESPACE_TO_CLIENT_INTERFACE = Map.ofEntries(
+                Map.entry("com.amazonaws.kms", "KmsClient"),
+                Map.entry("com.amazonaws.dynamodb", "DynamoDbClient"),
+                Map.entry("com.amazonaws.s3", "S3Client")
+        );
+        // These exception are located in the services' model package.
+        // i.e.: kms : software.amazon.awssdk.kms.model.KmsException
+        AWS_SERVICE_NAMESPACE_TO_BASE_EXCEPTION = Map.ofEntries(
+                Map.entry("com.amazonaws.kms", "KmsException"),
+                Map.entry("com.amazonaws.dynamodb", "DynamoDbException"),
+                Map.entry("com.amazonaws.s3", "S3Exception")
+        );
+    }
+
+    /** Validates that Polymorph knows non-smithy modeled constants for an AWS Service */
+    private void checkForAwsServiceConstants() {
+        String namespace = serviceShape.getId().getNamespace();
+        checkForAwsServiceConstants(namespace);
+    }
+
+    /** Validates that Polymorph knows non-smithy modeled constants for an AWS Service */
+    private static void checkForAwsServiceConstants(String namespace) {
+        boolean knowBaseException = AWS_SERVICE_NAMESPACE_TO_BASE_EXCEPTION.containsKey(namespace);
+        if (!knowBaseException) {
+            throw new IllegalArgumentException(
+                    "Polymorph does not know this service's Base Exception: %s".formatted(namespace));
+        }
+        boolean knowClientInterface = AWS_SERVICE_NAMESPACE_TO_CLIENT_INTERFACE.containsKey(namespace);
+        if (!knowClientInterface) {
+            throw new IllegalArgumentException(
+                    "Polymorph does not know this service's Client Interface: %s".formatted(namespace));
+        }
+    }
+
+    /**
+     * Throws IllegalArgumentException if shapeId is not in namespace
+     */
+    private void checkInServiceNamespace(final ShapeId shapeId) {
+        if (!isInServiceNameSpace(shapeId)) {
+            throw new IllegalArgumentException(
+                    "ShapeId %s is not in this namespace %s".formatted(
+                            shapeId, serviceShape.getId().getNamespace()));
+        }
+    }
+
+    public static ClassName classNameForServiceClient(ServiceShape shape) {
+        String awsServiceSmithyNamespace = shape.toShapeId().getNamespace();
+        checkForAwsServiceConstants(awsServiceSmithyNamespace);
+        return ClassName.get(
+                packageNameForAwsSdkV2Shape(shape),
+                AWS_SERVICE_NAMESPACE_TO_CLIENT_INTERFACE.get(awsServiceSmithyNamespace)
+        );
+    }
+
+    /**
+     * <p>In the AWS SDK Java V2,
+     * structures never return Enums, only their string representation.
+     * Thus, any methods that handle the result of a get Enum value
+     * must handle String, not the Enum reference.</p>
+     *
+     * <p>At this time, we believe that is only needs to be called
+     * for aggregates other than structure or union,
+     * as only Aggregate converters will indirectly deal with enums.</p>
+     *
+     * <p>Any direct involvement with Enums are safe,
+     * since we overload the enum converter methods.</p>
+     **/
+    private TypeName typeForShapeNoEnum(ShapeId shapeId) {
+        final Shape shape = model.expectShape(shapeId);
+
+        if (shape.hasTrait(EnumTrait.class)) {
+            if (shapeRequiresTypeConversionFromStringToStructure(shapeId)) {
+                return classForEnum(shape);
+            }
+
+            return classForString();
+        }
+        if (SHAPE_TYPES_LIST_SET_MAP.contains(shape.getType())) {
+            return typeForListSetOrMapNoEnum(shapeId);
+        }
+        return typeForShape(shapeId);
+    }
+
+    @Override
+    public TypeName typeForShape(final ShapeId shapeId) {
+        final Shape shape = model.expectShape(shapeId);
+
+        // Overrides BYTE shapeType type conversion to SdkBytes conversion.
+        if (shape.getType().equals(ShapeType.BYTE)) {
+            return JavaAwsSdkV2.BLOB_TO_NATIVE_SDK_BYTES;
+        }
+
+        // BinarySetAttributeValue is the only list of bytes
+        if (shapeId.getName().contains("BinarySetAttributeValue")) {
+            return ParameterizedTypeName.get(
+                ClassName.get(List.class),
+                JavaAwsSdkV2.BLOB_TO_NATIVE_SDK_BYTES);
+        }
+
+        return super.typeForShape(shapeId);
+    }
+
+    /**
+     * Returns true if the provided ShapeId has type string in the Smithy model, but AWS SDK for
+     *   Java V2 effectively expects type structure.
+     * @param shapeId
+     * @return true if AWS SDK for Java V2 expects this to have been modeled as a structure in Smithy
+     */
+    protected boolean shapeRequiresTypeConversionFromStringToStructure(
+        ShapeId shapeId) {
+        return shapeId.toString().contains("com.amazonaws.kms#EncryptionAlgorithmSpec")
+            || shapeId.toString().contains("com.amazonaws.kms#SigningAlgorithmSpec")
+            || shapeId.toString().contains("com.amazonaws.kms#GrantOperation");
+    }
+
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
+    public ParameterizedTypeName typeForListSetOrMapNoEnum(final ShapeId shapeId) {
+        final Shape shape = model.getShape(shapeId)
+                .orElseThrow(() -> new IllegalStateException("Cannot find shape " + shapeId));
+        return switch (shape.getType()) {
+            case LIST -> ParameterizedTypeName.get(
+                    ClassName.get(List.class),
+                    typeForShapeNoEnum(shape.asListShape().get().getMember().getTarget())
+            );
+            case SET -> ParameterizedTypeName.get(
+                    ClassName.get(Set.class),
+                    typeForShapeNoEnum(shape.asSetShape().get().getMember().getTarget())
+            );
+            case MAP -> ParameterizedTypeName.get(
+                    ClassName.get(Map.class),
+                    typeForShapeNoEnum(shape.asMapShape().get().getKey().getTarget()),
+                    typeForShapeNoEnum(shape.asMapShape().get().getValue().getTarget())
+            );
+            default -> throw new IllegalStateException(
+                    "typeForListSetOrMapNoEnum only accepts LIST, SET or MAP. Got: "
+                            + shape.getType() + " for ShapeId: " + shapeId);
+        };
+    }
+
+    /**
+     * Returns CodeBlock containing something like `member`.
+     * Most AWS SDK V2 setters are `uncapitalizedMemberName` with edge cases in this function
+     * @param shape
+     * @return CodeBlock containing something like `member`.
+     */
+    public CodeBlock fieldForSetMember(MemberShape shape) {
+        // Some strings have a token that requires multi-letter decapitalization (e.g. "SSE", "KMS")
+        String tokenToUncapitalize = tokenToUncapitalizeInShape(shape);
+        if (!tokenToUncapitalize.equals("")) {
+            return CodeBlock.of("$L", shape.getMemberName().replace(tokenToUncapitalize,
+                tokenToUncapitalize.toLowerCase()));
+        }
+        // Attributes of SDK AttributeValue shapes are always lower-case
+        if (shape.getContainer().getName().equals("AttributeValue")
+            && isAttributeValueType(shape)) {
+            // "NULL" attribute is set using "nul"
+            if (shape.getMemberName().equals("NULL")) {
+                return CodeBlock.of("nul");
+            }
+            return CodeBlock.of("$L", shape.getMemberName().toLowerCase());
+        }
+
+        return CodeBlock.of("$L", uncapitalize(shape.getMemberName()));
+    }
+
+    public static ClassName classNameForAwsSdkShape(final Shape shape) {
+        // Assume that the shape is in the model package
+        ClassName smithyName = ClassName.get(
+                defaultModelPackageName(packageNameForAwsSdkV2Shape(shape)),
+                StringUtils.capitalize(shape.getId().getName()));
+
+        if (smithyName.simpleName().endsWith("Input")) {
+            return ClassName.get(smithyName.packageName(),
+                    smithyName.simpleName()
+                            .substring(
+                                    0,
+                                    smithyName.simpleName().lastIndexOf("Input"))
+                            + "Request"
+            );
+        }
+
+        if (smithyName.simpleName().endsWith("Output")) {
+            return ClassName.get(smithyName.packageName(),
+                    smithyName.simpleName()
+                            .substring(
+                                    0,
+                                    smithyName.simpleName().lastIndexOf("Output"))
+                            + "Response"
+            );
+        }
+
+        if (shape.hasTrait(ErrorTrait.class)) {
+            if (smithyName.simpleName().contains("KMS")) {
+                return ClassName.get(smithyName.packageName(),
+                        smithyName.simpleName()
+                                .replace("KMS", "Kms")
+                );
+            }
+            if (smithyName.simpleName().contains("CMK")) {
+                return ClassName.get(smithyName.packageName(),
+                        smithyName.simpleName()
+                                .replace("CMK", "CmK")
+                );
+            }
+            if (smithyName.simpleName().endsWith("InternalServerError")) {
+                return ClassName.get(smithyName.packageName(),
+                        smithyName.simpleName()
+                                .replace("InternalServerError", "InternalServerErrorException")
+                );
+            }
+            if (smithyName.simpleName().endsWith("RequestLimitExceeded")) {
+                return ClassName.get(smithyName.packageName(),
+                        smithyName.simpleName()
+                                .replace("RequestLimitExceeded", "RequestLimitExceededException")
+                );
+            }
+        }
+        return smithyName;
+    }
+
+    @Override
+    public ClassName classNameForStructure(final Shape shape) {
+        if (!(shape.isUnionShape() || shape.isStructureShape())) {
+            throw new IllegalArgumentException(
+                    "typeForStructure should only be called for Structures or Unions. ShapeId: %s"
+                            .formatted(shape.getId()));
+        }
+        if (shape.hasTrait(TraitDefinition.class)) {
+            throw new IllegalArgumentException(
+                    "Trait definition structures have no corresponding generated type");
+        }
+        // check if this Shape is in AWS SDK for Java V2 package
+        if (AwsSdkNameResolverHelpers.isInAwsSdkNamespace(shape.getId())) {
+            AwsSdkNativeV2.classNameForAwsSdkShape(shape);
+        }
+        return super.classNameForStructure(shape);
+    }
+
+    public TypeName typeForOperationOutput(final ShapeId shapeId) {
+        StructureShape shape = model.expectShape(shapeId, StructureShape.class);
+        return classNameForStructure(shape);
+    }
+
+    public final String v2FormattedEnumValue(
+            // ShapeIds are great; every method in this class SHOULD accept a ShapeId.
+            // If we ever need to handle an exception to the enum naming pattern (which the AWS SDK is full of)
+            // The ShapeId will be used.
+            @SuppressWarnings("unused") final ShapeId shapeId,
+            final String enumValueName
+    ) {
+        // TODO: We SHOULD employ the awsSDkNaming for more of the logic above..
+        return this.awsSDKNaming.getEnumValueName(enumValueName);
+    }
+
+    /**
+     * Returns the TypeName for an AWS Service's Client Interface.
+     */
+    @Override
+    public ClassName classNameForService(final ServiceShape shape) {
+        //TODO: refactor to not throw error on other namespace,
+        // but instead check AWS_SERVICE_NAMESPACE_TO_CLIENT_INTERFACE for
+        // namespace, and throw exception if not found.
+        checkInServiceNamespace(shape.getId());
+        return ClassName.get(
+                packageName,
+                AWS_SERVICE_NAMESPACE_TO_CLIENT_INTERFACE.get(
+                        serviceShape.getId().getNamespace()));
+    }
+
+    /**
+     * Returns the TypeName for an AWS Service's Base Exception.
+     * <p>
+     * To be clear, a Base Exception is concrete.
+     * But all of a service's other exceptions extend it.
+     */
+    public ClassName baseErrorForService() {
+        return ClassName.get(
+                modelPackage,
+                AWS_SERVICE_NAMESPACE_TO_BASE_EXCEPTION.get(
+                        serviceShape.getId().getNamespace()));
+    }
+
+    public static String packageNameForService(final String awsServiceName) {
+        String rtn = awsServiceName;
+        return "software.amazon.awssdk.services.%s".formatted(rtn);
+    }
+
+    static String packageNameForAwsSdkV2Shape(final Shape shape) {
+        String awsServiceName = AwsSdkNameResolverHelpers.awsServiceNameFromShape(shape);
+        return packageNameForService(awsServiceName);
+    }
+
+    public static String defaultModelPackageName(final String packageName) {
+        return "%s.model".formatted(packageName);
+    }
+
+}
