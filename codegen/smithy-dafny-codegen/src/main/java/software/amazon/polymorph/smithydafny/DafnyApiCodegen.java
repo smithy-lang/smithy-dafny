241c081ef3c1a9df7877a2e5ec340cdafc8c021c
Tony Knapp <5892063+texastony@users.noreply.github.com>
2023-04-25 18:19:20 -0700

    typo

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithydafny/DafnyApiCodegen.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithydafny/DafnyApiCodegen.java
@@ -5,7 +5,6 @@ package software.amazon.polymorph.smithydafny;
 
 import com.google.common.annotations.VisibleForTesting;
 
-import software.amazon.polymorph.smithyjava.nameresolver.Dafny;
 import software.amazon.polymorph.traits.DafnyUtf8BytesTrait;
 import software.amazon.polymorph.traits.LocalServiceTrait;
 import software.amazon.polymorph.traits.MutableLocalStateTrait;
