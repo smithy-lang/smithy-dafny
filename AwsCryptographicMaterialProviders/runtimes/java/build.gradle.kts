529c7f464cb1eccc2b08db57f71a37018d172e82
Robin Salkeld <salkeldr@amazon.com>
2023-05-23 15:16:19 -0700

    chore: Switch to published conversion library (#14)

--- AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
+++ AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
@@ -54,7 +54,7 @@ repositories {
 
 dependencies {
     implementation("org.dafny:DafnyRuntime:4.1.0")
-    implementation("software.amazon.smithy.dafny:conversion:1.0-SNAPSHOT")
+    implementation("software.amazon.smithy.dafny:conversion:0.1")
     implementation("software.amazon.cryptography:StandardLibrary:1.0-SNAPSHOT")
     implementation("software.amazon.cryptography:AwsCryptographyPrimitives:1.0-SNAPSHOT")
     implementation("software.amazon.cryptography:ComAmazonawsKms:1.0-SNAPSHOT")
--- TestVectorsAwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
@@ -57,7 +57,7 @@ repositories {
 
 dependencies {
     implementation("org.dafny:DafnyRuntime:4.1.0")
-    implementation("software.amazon.smithy.dafny:conversion:1.0-SNAPSHOT")
+    implementation("software.amazon.smithy.dafny:conversion:0.1")
     implementation("software.amazon.cryptography:AwsCryptographicMaterialProviders:1.0-SNAPSHOT")
     implementation(platform("software.amazon.awssdk:bom:2.19.1"))
     implementation("software.amazon.awssdk:dynamodb")
