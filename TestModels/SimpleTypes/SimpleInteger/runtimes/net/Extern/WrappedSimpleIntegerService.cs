e589f886f54080a16e526ec898e2437a547014ff
Robin Salkeld <salkeldr@amazon.com>
2023-06-07 09:02:18 -0700

    More missing headers

--- TestModels/SimpleTypes/SimpleInteger/runtimes/net/Extern/WrappedSimpleIntegerService.cs
+++ TestModels/SimpleTypes/SimpleInteger/runtimes/net/Extern/WrappedSimpleIntegerService.cs
@@ -1,3 +1,5 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 using Wrappers_Compile;
 using Simple.Types.Integer;
 using Simple.Types.Integer.Wrapped;
