b006f11f0127f1a52a228ad4bb3893b8e58c2b61
Lucas McDonald <lucmcdon@amazon.com>
2023-06-13 15:15:21 -0700

    add src-goal

---
+++ TestModels/SimpleTypes/SimpleBoolean/runtimes/python/src-goal/dafny_generated/Extern.py
@@ -0,0 +1,22 @@
+import sys
+from typing import Callable, Any, TypeVar, NamedTuple
+from math import floor
+from itertools import count
+
+import module_
+import _dafny
+import System_
+import SimpleBooleanImplTest_Compile
+import simple.types.boolean.internaldafny.types
+from python_client_codegen.simple_boolean.client import SimpleBoolean
+from Shim import SimpleBooleanShim
+import Wrappers_Compile
+
+@staticmethod
+def WrappedSimpleBoolean(config):
+    wrapped_config = config
+    impl = SimpleBoolean(wrapped_config)
+    wrapped_client = SimpleBooleanShim(impl)
+    return Wrappers_Compile.Result_Success(wrapped_client)
+
+simple.types.boolean.internaldafny.wrapped.default__.WrappedSimpleBoolean = WrappedSimpleBoolean
---
+++ TestModels/SimpleTypes/SimpleBoolean/runtimes/python/src-goal/dafny_generated/module_.py
@@ -0,0 +1,80 @@
+import sys
+from typing import Callable, Any, TypeVar, NamedTuple
+from math import floor
+from itertools import count
+
+import module_
+import _dafny
+import System_
+import SimpleBooleanImplTest_Compile
+import simple.types.boolean.internaldafny.wrapped
+import WrappedSimpleTypesBooleanTest_Compile
+
+assert "module_" == __name__
+module_ = sys.modules[__name__]
+
+class default__:
+    def  __init__(self):
+        pass
+
+    def __dafnystr__(self) -> str:
+        return "_module._default"
+    @staticmethod
+    def Test____Main____(noArgsParameter__):
+        d_12_success_: bool
+        d_12_success_ = True
+        _dafny.print(_dafny.string_of(_dafny.Seq("SimpleBooleanImplTest.GetBooleanTrue: ")))
+        try:
+            if True:
+                SimpleBooleanImplTest_Compile.default__.GetBooleanTrue()
+                if True:
+                    _dafny.print(_dafny.string_of(_dafny.Seq("PASSED\n")))
+        except _dafny.HaltException as e:
+            d_13_haltMessage_ = e.message
+            if True:
+                _dafny.print(_dafny.string_of(_dafny.Seq("FAILED\n	")))
+                _dafny.print(_dafny.string_of(d_13_haltMessage_))
+                _dafny.print(_dafny.string_of(_dafny.Seq("\n")))
+                d_12_success_ = False
+        _dafny.print(_dafny.string_of(_dafny.Seq("SimpleBooleanImplTest.GetBooleanFalse: ")))
+        try:
+            if True:
+                SimpleBooleanImplTest_Compile.default__.GetBooleanFalse()
+                if True:
+                    _dafny.print(_dafny.string_of(_dafny.Seq("PASSED\n")))
+        except _dafny.HaltException as e:
+            d_14_haltMessage_ = e.message
+            if True:
+                _dafny.print(_dafny.string_of(_dafny.Seq("FAILED\n	")))
+                _dafny.print(_dafny.string_of(d_14_haltMessage_))
+                _dafny.print(_dafny.string_of(_dafny.Seq("\n")))
+                d_12_success_ = False
+        _dafny.print(_dafny.string_of(_dafny.Seq("WrappedSimpleTypesBooleanTest.GetBooleanTrue: ")))
+        try:
+            if True:
+                WrappedSimpleTypesBooleanTest_Compile.default__.GetBooleanTrue()
+                if True:
+                    _dafny.print(_dafny.string_of(_dafny.Seq("PASSED\n")))
+        except _dafny.HaltException as e:
+            d_15_haltMessage_ = e.message
+            if True:
+                _dafny.print(_dafny.string_of(_dafny.Seq("FAILED\n	")))
+                _dafny.print(_dafny.string_of(d_15_haltMessage_))
+                _dafny.print(_dafny.string_of(_dafny.Seq("\n")))
+                d_12_success_ = False
+        _dafny.print(_dafny.string_of(_dafny.Seq("WrappedSimpleTypesBooleanTest.GetBooleanFalse: ")))
+        try:
+            if True:
+                WrappedSimpleTypesBooleanTest_Compile.default__.GetBooleanFalse()
+                if True:
+                    _dafny.print(_dafny.string_of(_dafny.Seq("PASSED\n")))
+        except _dafny.HaltException as e:
+            d_16_haltMessage_ = e.message
+            if True:
+                _dafny.print(_dafny.string_of(_dafny.Seq("FAILED\n	")))
+                _dafny.print(_dafny.string_of(d_16_haltMessage_))
+                _dafny.print(_dafny.string_of(_dafny.Seq("\n")))
+                d_12_success_ = False
+        if not(d_12_success_):
+            raise _dafny.HaltException("/Users/lucmcdon/Desktop/workplace/polymorph/TestModels/SimpleTypes/SimpleBoolean/test/SimpleBooleanImplTest.dfy(3,0): " + _dafny.string_of(_dafny.Seq("Test failures occurred: see above.\n")))
+
