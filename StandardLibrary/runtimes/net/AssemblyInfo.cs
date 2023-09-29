b3e66ff4d103e9e206c0d8ca981c2940329fcce0
José Corella <39066999+josecorella@users.noreply.github.com>
2023-09-29 14:56:00 -0700

    chore: update StandardLibrary to sign net48 dlls (#76)

--- StandardLibrary/runtimes/net/AssemblyInfo.cs
+++ StandardLibrary/runtimes/net/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyTitle("AWS.Cryptography.Internal.StandardLibrary")]
 
 // This should be kept in sync with the version number in STD.csproj
-[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyVersion("1.1.1")]
