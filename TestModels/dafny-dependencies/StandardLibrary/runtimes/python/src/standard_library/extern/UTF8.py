cb030b2a11ac12facc1edaf76cb37b902658de53
Lucas McDonald <lucmcdon@amazon.com>
2023-10-02 15:38:02 -0700

    clarify

--- TestModels/dafny-dependencies/StandardLibrary/runtimes/python/src/standard_library/extern/UTF8.py
+++ TestModels/dafny-dependencies/StandardLibrary/runtimes/python/src/standard_library/extern/UTF8.py
@@ -8,7 +8,7 @@ Extern UTF8 encode and decode methods.
 
 Note:
 Python's `.encode('utf-8')` does not handle surrogates.
-These methods are expected to handle surrogates (e.g. "\uD808\uDC00").
+However, these encode/decode methods are expected to handle surrogates (e.g. "\uD808\uDC00").
 To work around this, we encode Dafny strings into UTF-16-LE (little endian)
 and decode them before encoding into UTF-8 (`_strict_tostring`).
 To decode, we reverse the encode step. (`_reverse_strict_tostring`)
@@ -29,31 +29,31 @@ class default__(standard_library.internal_generated_dafny.UTF8.default__):
       return Wrappers.Result_Failure(_dafny.Seq("Could not encode input to Dafny Bytes."))
 
   @staticmethod
-  def _strict_tostring(s):
+  def _strict_tostring(dafny_ascii_string):
     '''
-    Converts a Dafny Seq of ASCII characters
-    into a string by writing a UTF-16-LE decoding,
-    then decoding it to ASCII with `strict` encoding.
+    Converts a Dafny Seq of unicode-escaped ASCII characters
+    into a string that can be encoded with Python's built-in `.encode('utf-8')`.
 
-    This encoding-decoding allows subsequent ASCII-to-UTF8 encodings
+    This encoding-decoding allows subsequent UTF8 encodings
     to handle surrogates as expected by Dafny code.
 
-    This is exactly the `_dafny.string_from_utf_16` method, except with
-    `errors = 'strict'` in our code
+    This is exactly the `_dafny.string_from_utf_16` method from the DafnyRuntime, except with
+    `errors = 'strict'` here,
     instead of
     `errors = 'replace'` in the `_dafny.string_from_utf_16` function.
     `strict` will throw an exception for invalid encodings, allowing us
-    to detect invalid encodings and raise exceptions.
+    to detect invalid encodings and raise exceptions,
+    while `replace` will fail silently.
     :param s:
     :return:
     '''
-    return b''.join(ord(c).to_bytes(2, 'little') for c in s).decode("utf-16-le", errors = 'strict')
+    return b''.join(ord(c).to_bytes(2, 'little') for c in dafny_ascii_string).decode("utf-16-le", errors = 'strict')
 
   @staticmethod
   def Decode(s):
     try:
-      a = bytes(s).decode('utf-8')
-      out = default__._reverse_strict_tostring(a)
+      utf8_str = bytes(s).decode('utf-8')
+      unicode_escaped_utf8_str = default__._reverse_strict_tostring(utf8_str)
       return Wrappers.Result_Success(out)
     # Catch both Encode and Decode errors.
     # The `try` block involves both encoding and decoding.
@@ -62,25 +62,25 @@ class default__(standard_library.internal_generated_dafny.UTF8.default__):
 
 
   @staticmethod
-  def _reverse_strict_tostring(s):
+  def _reverse_strict_tostring(utf8_str):
     '''
-    Converts a string into a Dafny Seq of ASCII characters.
-    This will undo the `_strict_tostring` function in this file.
+    Converts a string into a Dafny Seq of unicode-escaped ASCII characters.
+    This is the inverse of the `_strict_tostring` function in this file.
     :param s:
     :return:
     '''
-    b = s.encode("utf-16-le", errors = "strict")
+    utf16_bytes = utf8_str.encode("utf-16-le", errors = "strict")
     out = []
     # len(b)/2 is an integer by construction of UTF-16 encoding (2 bytes per encoded character)
-    for i in range(int(len(b)/2)):
+    for i in range(int(len(utf16_bytes)/2)):
       # Take two consecutive bytes;
-      c = b[2*i:2*i+2]
+      utf_16_bytepair = utf16_bytes[2*i:2*i+2]
       # Unpack them into an ordinal representation;
-      d = struct.unpack('<H', c)
+      packed_bytes = struct.unpack('<H', utf_16_bytepair)
       # Convert into a character representation;
-      e = chr(d[0])
+      char_representation = chr(packed_bytes[0])
       # Append to returned string
-      out.append(e)
+      out.append(char_representation)
     return _dafny.Seq(out)
 
 standard_library.internal_generated_dafny.UTF8.default__ = default__
