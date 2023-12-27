d61d6850222773676d1bca5a79eb5fe8eebafcb6
Lucas McDonald <lucasmcdonald3@gmail.com>
2023-12-27 11:00:24 -0800

    missing'

---
+++ TestModels/Dependencies/runtimes/python/test/internaldafny/extern/wrapped_simple_dependencies.py
@@ -0,0 +1,23 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import simple_dependencies_internaldafny
+import simple_dependencies_internaldafny_wrapped
+from simple_dependencies.smithygenerated.simple_dependencies.client import SimpleDependencies
+from simple_dependencies.smithygenerated.simple_dependencies.config import dafny_config_to_smithy_config
+from simple_dependencies.smithygenerated.simple_dependencies.shim import SimpleDependenciesShim
+import Wrappers
+
+@staticmethod
+def WrappedSimpleDependencies(config):
+    wrapped_config = dafny_config_to_smithy_config(config)
+    impl = SimpleDependencies(wrapped_config)
+    # TODO: Generate an alternate constructor for Smithy client that does not take in a config,
+    # but instead takes in an extern-supplied Dafny client
+    # and also converts its Config to the Smithy config
+    # SIM: https://sim.amazon.com/issues/CrypTool-5230
+    impl._config.dafnyImplInterface.impl = simple_dependencies_internaldafny.default__.SimpleDependencies(config).value
+    wrapped_client = SimpleDependenciesShim(impl)
+    return Wrappers.Result_Success(wrapped_client)
+
+simple_dependencies_internaldafny_wrapped.default__.WrappedSimpleDependencies = WrappedSimpleDependencies
---
+++ TestModels/LocalService/runtimes/python/test/internaldafny/extern/wrapped_simple_localservice.py
@@ -0,0 +1,21 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# TODO-Python-PYTHONPATH: Qualify imports
+import simple_localservice_internaldafny_wrapped
+from simple_localservice.smithygenerated.simple_localservice.client import SimpleLocalService
+from simple_localservice.smithygenerated.simple_localservice.shim import SimpleLocalServiceShim
+from simple_localservice.smithygenerated.simple_localservice.config import dafny_config_to_smithy_config
+import Wrappers
+
+class default__(simple_localservice_internaldafny_wrapped.default__):
+
+    @staticmethod
+    def WrappedSimpleLocalService(config):
+        wrapped_config = dafny_config_to_smithy_config(config)
+        impl = SimpleLocalService(wrapped_config)
+        wrapped_client = SimpleLocalServiceShim(impl)
+        return Wrappers.Result_Success(wrapped_client)
+
+# (TODO-Python-PYTHONPATH: Remove)
+simple_localservice_internaldafny_wrapped.default__ = default__
