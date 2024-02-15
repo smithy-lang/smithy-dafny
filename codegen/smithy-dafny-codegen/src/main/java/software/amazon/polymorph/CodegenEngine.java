11869302912f2066db54302fe6c5f4b661778d1a
Robin Salkeld <salkeldr@amazon.com>
2024-02-15 15:24:36 -0800

    Apply patches after updating them

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/CodegenEngine.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/CodegenEngine.java
@@ -351,7 +351,9 @@ public class CodegenEngine {
                 if (!patchContent.isBlank()) {
                     IOUtils.writeToFile(patchContent, patchFile.toFile());
                 }
-            } else if (Files.exists(patchFilesDir)) {
+            }
+
+            if (Files.exists(patchFilesDir)) {
                 List<Pair<DafnyVersion, Path>> sortedPatchFiles = Files.list(patchFilesDir)
                         .map(file -> Pair.of(getDafnyVersionForPatchFile(file), file))
                         .sorted(Collections.reverseOrder(Map.Entry.comparingByKey()))
