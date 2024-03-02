fcff59986508488234b75e724eb38de6bf4496cb
Robin Salkeld <salkeldr@amazon.com>
2024-03-02 10:23:09 -0800

    License header

--- TestModels/SharedMakefile.mk
+++ TestModels/SharedMakefile.mk
@@ -1,3 +1,5 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 PROJECT_ROOT := $(abspath $(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
 
