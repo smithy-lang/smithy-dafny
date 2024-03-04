8728872f89d2850ded53a3c2739d38be2440290c
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-03-04 12:56:10 -0800

    wip

--- TestModels/LanguageSpecificLogic/runtimes/python/src/language_specific_logic/internaldafny/extern/language_specific_logic_externs.py
+++ TestModels/LanguageSpecificLogic/runtimes/python/src/language_specific_logic/internaldafny/extern/language_specific_logic_externs.py
@@ -1,13 +1,13 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-import PythonLanguageSpecificLogicImpl
+import LanguageSpecificLogicImpl
 import sys
 import Wrappers
 import _dafny
 
-class default__(PythonLanguageSpecificLogicImpl.default__):
+class default__(LanguageSpecificLogicImpl.default__):
     @staticmethod
     def GetPythonRuntimeVersion(config):
         return Wrappers.Result_Success(_dafny.Seq(str(sys.version)))
 
-PythonLanguageSpecificLogicImpl.default__ = default__
+LanguageSpecificLogicImpl.default__ = default__
