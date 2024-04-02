632c35da5c479ce3d60e79f2e913aff6f5f1b06f
Robin Salkeld <salkeldr@amazon.com>
2024-04-02 11:16:11 -0700

    Partial work switching to tokio::test

--- TestModels/SimpleTypes/SimpleString/runtimes-test/rust/src/lib.rs
+++ TestModels/SimpleTypes/SimpleString/runtimes-test/rust/src/lib.rs
@@ -2,7 +2,6 @@
 #![allow(nonstandard_style)]
 extern crate simple_string;
 
-#[cfg(test)]
 mod SimpleStringImplTest {
   use simple_string::*;
   /*
@@ -14,30 +13,23 @@ mod SimpleStringImplTest {
     }
   */
 
-  #[test]
-  fn GetString() {
-    let config = Config::builder().build();
-    let client = Client::from_conf(config);
-    TestGetString(&client);
-    TestGetStringSingleValue(&client);
-    TestGetStringUTF8(&client);
-  }
-
   /*method TestGetString(client: ISimpleTypesStringClient)
     {
         var ret :- expect client.GetString(SimpleString.Types.GetStringInput(value:= Some("TEST_SIMPLE_STRING_VALUE")));
         expect ret.value.UnwrapOr("") == "TEST_SIMPLE_STRING_VALUE";
         print ret;
     } */
-  async fn TestGetString(client: &Client) {
-    let result = client.get_string().value("TEST_SIMPLE_STRING_VALUE").send().await;
+  #[tokio::test]
+  async fn test_get_string() {
+    let result = client().get_string().value("TEST_SIMPLE_STRING_VALUE").send().await;
     let output = result.unwrap();
     let value = output.value().unwrap();
     assert_eq!(value, "TEST_SIMPLE_STRING_VALUE");
   }
 
-  async fn TestGetStringSingleValue(client: &Client) {
-    let result = client.get_string().value("TEST_SIMPLE_STRING_VALUE").send().await;
+  #[tokio::test]
+  async fn test_get_string_single_value() {
+    let result = client().get_string().value("TEST_SIMPLE_STRING_VALUE").send().await;
     let output = result.unwrap();
     let value = output.value().unwrap();
     assert_eq!(value, "TEST_SIMPLE_STRING_SINGLE_VALUE");
@@ -51,11 +43,17 @@ mod SimpleStringImplTest {
         expect ret.value.UnwrapOr("") == utf8EncodedString;
         print ret;
     }*/
-  async fn TestGetStringUTF8(client: &Client) {
+  #[tokio::test]
+  async fn test_get_string_utf8() {
     let utf8EncodedString = "\u{0905}\u{0928}\u{093e}\u{0930}";
-    let result = client.get_string().value(utf8EncodedString).send().await;
+    let result = client().get_string().value(utf8EncodedString).send().await;
     let output = result.unwrap();
     let value = output.value().unwrap();
     assert_eq!(value, utf8EncodedString);
+  }
+
+  pub fn client() -> Client {
+    let config = Config::builder().build();
+    Client::from_conf(config)
   }
 }
