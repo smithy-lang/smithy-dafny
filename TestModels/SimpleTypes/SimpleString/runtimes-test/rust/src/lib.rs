c12da8b74907334386ab766bdf627b9cd083f0ab
Robin Salkeld <salkeldr@amazon.com>
2024-04-02 10:32:24 -0700

    Refactor test to only use nice Client interface

--- TestModels/SimpleTypes/SimpleString/runtimes-test/rust/src/lib.rs
+++ TestModels/SimpleTypes/SimpleString/runtimes-test/rust/src/lib.rs
@@ -5,7 +5,6 @@ extern crate simple_string;
 #[cfg(test)]
 mod SimpleStringImplTest {
   use simple_string::*;
-  use simple_string::implementation_from_dafny::dafny_runtime;
   /*
   method{:test} GetString(){
         var client :- expect SimpleString.SimpleString();
@@ -17,15 +16,11 @@ mod SimpleStringImplTest {
 
   #[test]
   fn GetString() {
-    let _r =
-      simple_string::implementation_from_dafny::r#_simple_dtypes_dsmithystring_dinternaldafny::_default::SimpleString(
-        &::std::rc::Rc::new(simple_string::implementation_from_dafny::r#_simple_dtypes_dsmithystring_dinternaldafny::_default::DefaultSimpleStringConfig())
-      );
-    assert!(!_r.IsFailure());
-    let client = _r.Extract();
-    TestGetString(client);
-    TestGetStringSingleValue(client);
-    TestGetStringUTF8(client);
+    let config = Config::builder().build();
+    let client = Client::from_conf(config);
+    TestGetString(&client);
+    TestGetStringSingleValue(&client);
+    TestGetStringUTF8(&client);
   }
 
   /*method TestGetString(client: ISimpleTypesStringClient)
@@ -34,30 +29,18 @@ mod SimpleStringImplTest {
         expect ret.value.UnwrapOr("") == "TEST_SIMPLE_STRING_VALUE";
         print ret;
     } */
-  fn TestGetString(client: *mut dyn simple_string::implementation_from_dafny::r#_simple_dtypes_dsmithystring_dinternaldafny_dtypes::ISimpleTypesStringClient) {
-    let _r = dafny_runtime::read!(client).GetString(
-      &::std::rc::Rc::new(simple_string::implementation_from_dafny::r#_simple_dtypes_dsmithystring_dinternaldafny_dtypes::GetStringInput::GetStringInput {
-        value: ::std::rc::Rc::new(
-          simple_string::implementation_from_dafny::_Wrappers_Compile::Option::Some { value: dafny_runtime::string_utf16_of("TEST_SIMPLE_STRING_VALUE") })
-      }),
-    );
-    assert!(!_r.IsFailure());
-    let ret = _r.Extract();
-    assert_eq!(ret.value().UnwrapOr(&dafny_runtime::string_utf16_of("")), dafny_runtime::string_utf16_of("TEST_SIMPLE_STRING_VALUE"));
-    println!("{:?}", ret);
+  async fn TestGetString(client: &Client) {
+    let result = client.get_string().value("TEST_SIMPLE_STRING_VALUE").send().await;
+    let output = result.unwrap();
+    let value = output.value().unwrap();
+    assert_eq!(value, "TEST_SIMPLE_STRING_VALUE");
   }
 
-  fn TestGetStringSingleValue(client: *mut dyn simple_string::implementation_from_dafny::r#_simple_dtypes_dsmithystring_dinternaldafny_dtypes::ISimpleTypesStringClient) {
-    let _r = dafny_runtime::read!(client).GetStringSingleValue(
-      &::std::rc::Rc::new(simple_string::implementation_from_dafny::r#_simple_dtypes_dsmithystring_dinternaldafny_dtypes::GetStringInput::GetStringInput {
-        value: ::std::rc::Rc::new(
-          simple_string::implementation_from_dafny::_Wrappers_Compile::Option::Some { value: dafny_runtime::string_utf16_of("TEST_SIMPLE_STRING_SINGLE_VALUE") })
-      }),
-    );
-    assert!(!_r.IsFailure());
-    let ret = _r.Extract();
-    assert_eq!(ret.value().UnwrapOr(&dafny_runtime::string_utf16_of("")), dafny_runtime::string_utf16_of("TEST_SIMPLE_STRING_SINGLE_VALUE"));
-    println!("{:?}", ret);
+  async fn TestGetStringSingleValue(client: &Client) {
+    let result = client.get_string().value("TEST_SIMPLE_STRING_VALUE").send().await;
+    let output = result.unwrap();
+    let value = output.value().unwrap();
+    assert_eq!(value, "TEST_SIMPLE_STRING_SINGLE_VALUE");
   }
 
    /*method TestGetStringUTF8(client: ISimpleTypesStringClient)
@@ -68,17 +51,11 @@ mod SimpleStringImplTest {
         expect ret.value.UnwrapOr("") == utf8EncodedString;
         print ret;
     }*/
-  fn TestGetStringUTF8(client: *mut dyn simple_string::implementation_from_dafny::r#_simple_dtypes_dsmithystring_dinternaldafny_dtypes::ISimpleTypesStringClient) {
-    let utf8EncodedString = dafny_runtime::string_utf16_of("\u{0905}\u{0928}\u{093e}\u{0930}");
-    let _r = dafny_runtime::read!(client).GetStringUTF8(
-      &::std::rc::Rc::new(simple_string::implementation_from_dafny::r#_simple_dtypes_dsmithystring_dinternaldafny_dtypes::GetStringInput::GetStringInput {
-        value: ::std::rc::Rc::new(
-          simple_string::implementation_from_dafny::_Wrappers_Compile::Option::Some { value: utf8EncodedString.clone() })
-      }),
-    );
-    assert!(!_r.IsFailure());
-    let ret = _r.Extract();
-    assert_eq!(ret.clone().value().UnwrapOr(&dafny_runtime::string_utf16_of("")), utf8EncodedString.clone());
-    println!("{:?}", ret);
+  async fn TestGetStringUTF8(client: &Client) {
+    let utf8EncodedString = "\u{0905}\u{0928}\u{093e}\u{0930}";
+    let result = client.get_string().value(utf8EncodedString).send().await;
+    let output = result.unwrap();
+    let value = output.value().unwrap();
+    assert_eq!(value, utf8EncodedString);
   }
 }
