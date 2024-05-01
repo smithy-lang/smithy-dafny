a2ad26aed161ff4b7ec8ae6401285e6c7e90d5cf
J Plasmeier <76071473+justplaz@users.noreply.github.com>
2024-05-01 10:30:54 -0700

    chore: add SimpleEnumV2 TestModel for Python (#346)
    * chore: add TestModel for SimpleEnumV2 in Python
    * chore: enable SimpleEnumV2 Python in CI

---
+++ TestModels/SimpleTypes/SimpleEnumV2/runtimes/python/test/internaldafny/extern/wrapped_simple_enum.py
@@ -0,0 +1,21 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# TODO-Python-PYTHONPATH: Qualify imports
+import simple_types_enumv2_internaldafny_wrapped
+from simple_types_smithyenumv2.smithygenerated.simple_types_enumv2.client import SimpleTypesEnumV2
+from simple_types_smithyenumv2.smithygenerated.simple_types_enumv2.shim import SimpleEnumV2Shim
+from simple_types_smithyenumv2.smithygenerated.simple_types_enumv2.config import dafny_config_to_smithy_config
+import Wrappers
+
+class default__(simple_types_enumv2_internaldafny_wrapped.default__):
+
+    @staticmethod
+    def WrappedSimpleEnumV2(config):
+        wrapped_config = dafny_config_to_smithy_config(config)
+        impl = SimpleTypesEnumV2(wrapped_config)
+        wrapped_client = SimpleEnumV2Shim(impl)
+        return Wrappers.Result_Success(wrapped_client)
+
+# (TODO-Python-PYTHONPATH: Remove)
+simple_types_enumv2_internaldafny_wrapped.default__ = default__
