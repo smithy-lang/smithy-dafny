2d645992d40b72e9c30894b44581f5a12c4b71c6
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-05-14 15:24:56 -0700

    also extern

---
+++ TestModels/Extern/runtimes/python/test/internaldafny/test_dafny_wrapper.py
@@ -0,0 +1,20 @@
+"""
+Wrapper file for executing Dafny tests from pytest.
+This allows us to import modules required by Dafny-generated tests
+before executing Dafny-generated tests.
+pytest will find and execute the `test_dafny` method below,
+which will execute the `internaldafny_test_executor.py` file in the `dafny` directory.
+"""
+
+import sys
+
+internaldafny_dir = '/'.join(__file__.split("/")[:-1])
+
+sys.path.append(internaldafny_dir + "/extern")
+sys.path.append(internaldafny_dir + "/generated")
+
+from .extern import wrapped_simple_extern
+
+def test_dafny():
+  # Dafny writes Dafny tests to __main__
+  from .generated import __main__
