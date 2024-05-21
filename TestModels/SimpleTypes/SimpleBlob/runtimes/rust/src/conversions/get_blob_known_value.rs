c429adce0cf09b0547b657c63302ccccd1cc006b
Andrew Banchich <banchich@amazon.com>
2024-05-21 15:51:42 -0400

    remove whitespace

--- TestModels/SimpleTypes/SimpleBlob/runtimes/rust/src/conversions/get_blob_known_value.rs
+++ TestModels/SimpleTypes/SimpleBlob/runtimes/rust/src/conversions/get_blob_known_value.rs
@@ -3,17 +3,23 @@
 use std::any::Any;
 
 #[allow(dead_code)]
-pub fn to_dafny_error(value: crate::operation::get_blob_known_value::GetBlobKnownValueError) -> ::std::rc::Rc<::simple_blob_dafny::r#_simple_dtypes_dsmithyblob_dinternaldafny_dtypes::Error> {
-  match value {
-    crate::operation::get_blob_known_value::GetBlobKnownValueError::Unhandled(unhandled) => 
+pub fn to_dafny_error(
+    value: crate::operation::get_blob_known_value::GetBlobKnownValueError,
+) -> ::std::rc::Rc<::simple_blob_dafny::r#_simple_dtypes_dsmithyblob_dinternaldafny_dtypes::Error> {
+    match value {
+    crate::operation::get_blob_known_value::GetBlobKnownValueError::Unhandled(unhandled) =>
       ::std::rc::Rc::new(::simple_blob_dafny::r#_simple_dtypes_dsmithyblob_dinternaldafny_dtypes::Error::Opaque { obj: Box::into_raw(Box::new(unhandled) as Box<dyn Any>) })
   }
 }
 
 #[allow(dead_code)]
-pub fn from_dafny_error(dafny_value: ::std::rc::Rc<::simple_blob_dafny::r#_simple_dtypes_dsmithyblob_dinternaldafny_dtypes::Error>) -> crate::operation::get_blob_known_value::GetBlobKnownValueError {
-  // TODO: Losing information here, but we have to figure out how to wrap an arbitrary Dafny value as std::error::Error
-  if matches!(&dafny_value.as_ref(), ::simple_blob_dafny::r#_simple_dtypes_dsmithyblob_dinternaldafny_dtypes::Error::CollectionOfErrors { .. }) {
+pub fn from_dafny_error(
+    dafny_value: ::std::rc::Rc<
+        ::simple_blob_dafny::r#_simple_dtypes_dsmithyblob_dinternaldafny_dtypes::Error,
+    >,
+) -> crate::operation::get_blob_known_value::GetBlobKnownValueError {
+    // TODO: Losing information here, but we have to figure out how to wrap an arbitrary Dafny value as std::error::Error
+    if matches!(&dafny_value.as_ref(), ::simple_blob_dafny::r#_simple_dtypes_dsmithyblob_dinternaldafny_dtypes::Error::CollectionOfErrors { .. }) {
     let error_message = "TODO: can't get message yet";
     crate::operation::get_blob_known_value::GetBlobKnownValueError::generic(::aws_smithy_types::error::metadata::ErrorMetadata::builder().message(error_message).build())
   } else {
