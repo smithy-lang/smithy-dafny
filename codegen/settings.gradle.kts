e6fd772d2a272021e1ebe652b98de50320fd11c0
Robin Salkeld <salkeldr@amazon.com>
2024-05-21 17:59:10 -0700

    Add submodule, start to make Gradle work

--- codegen/settings.gradle.kts
+++ codegen/settings.gradle.kts
@@ -5,6 +5,8 @@ rootProject.name = "smithy-dafny"
 include(":smithy-dafny-codegen")
 include(":smithy-dafny-codegen-cli")
 //include(":smithy-dafny-codegen-test")
+include(":smithy-rust-client-codegen")
+project(":smithy-rust-client-codegen").projectDir = file("../smithy-dafny-codegen-modules/smithy-rs/codegen-client")
 
 pluginManagement {
     repositories {
