699d82bcb29f094e8bddd45a086624c60f551704
Andy Jewell <ajewell@amazon.com>
2024-06-05 16:28:12 -0400

    format

--- TestModels/SimpleTypes/SimpleBoolean/runtimes/rust/src/operation/get_boolean.rs
+++ TestModels/SimpleTypes/SimpleBoolean/runtimes/rust/src/operation/get_boolean.rs
@@ -16,7 +16,8 @@ impl GetBoolean {
         crate::operation::get_boolean::GetBooleanError,
     > {
         let inner_input = crate::conversions::get_boolean::_get_boolean_input::to_dafny(input);
-        let inner_result = ::dafny_runtime::md!(client.dafny_client.clone()).GetBoolean(&inner_input);
+        let inner_result =
+            ::dafny_runtime::md!(client.dafny_client.clone()).GetBoolean(&inner_input);
         if matches!(
             inner_result.as_ref(),
             ::simple_boolean_dafny::r#_Wrappers_Compile::Result::Success { .. }
