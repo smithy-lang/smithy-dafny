a8a8870fede88ecf606cf5d05a8045207757fc80
Andy Jewell <ajewell@amazon.com>
2024-06-18 11:57:47 -0700

    m

--- TestModels/Extendable/runtimes/rust/tests/extendable_resources_test.rs
+++ TestModels/Extendable/runtimes/rust/tests/extendable_resources_test.rs
@@ -1,9 +1,10 @@
 use simple_extendable::operation::get_extendable_resource_data::*;
+use simple_extendable::operation::create_extendable_resource::*;
 use simple_extendable::types::extendable_resource::ExtendableResourceRef;
 use simple_extendable::types::*;
 use simple_extendable::*;
 
-// const TEST_RESOURCE_NAME : &str = "Dafny-Test";
+const TEST_RESOURCE_NAME : &str = "Dafny-Test";
 const DEFAULT_RESOURCE_NAME: &str = "dafny-default";
 
 struct MyResource {}
@@ -37,6 +38,91 @@ pub fn DafnyFactory() -> ExtendableResourceRef {
     std::rc::Rc::new(std::cell::RefCell::new(MyResource {}))
 }
 
+
+  // Tests the Resource created purely through Dafny Source Code
+  #[tokio::test]
+  pub async fn TestClientDafnyResource()
+  {
+    let config = SimpleExtendableResourcesConfig::builder().build().unwrap();
+    let client = Client::from_conf(config.clone()).unwrap();
+
+    // The explicit type cast is needed for the `is` test on the next line
+    let resource = TestCreateExtendableResource(
+      &client, TEST_RESOURCE_NAME
+    ).await;
+    // expect resource is ExtendableResource.ExtendableResource;
+    // The `is` test above asserts this a "pure" Dafny resource
+    TestNoneUseExtendableResource(&client, resource.clone(), TEST_RESOURCE_NAME);
+    TestSomeUseExtendableResource(&client, resource.clone(), TEST_RESOURCE_NAME);
+    // TestUseAlwaysModeledError(client, resource);
+    // TestUseAlwaysMultipleErrors(client, resource);
+    // TestDafnyUseAlwaysOpaqueError(client, resource);
+  }
+
+//   // Test the Resource created through an Extern
+//   #[tokio::test]
+//   pub fn TestClientNativeResource()
+//   {
+//     let client = SimpleExtendableResources::builder().build();
+//     // The explicit type cast is needed for the `is` test on the next line
+//     var resource: Types.IExtendableResource := DafnyFactory();
+//     // expect !(resource is ExtendableResource.ExtendableResource);
+//     // The `is` test above asserts this NOT a "pure" Dafny resource
+//     assert fresh(resource.Modifies - client.Modifies - {client.History});
+//     TestNoneUseExtendableResource(client, resource, ExtendableResource.DEFAULT_RESOURCE_NAME);
+//     TestSomeUseExtendableResource(client, resource, ExtendableResource.DEFAULT_RESOURCE_NAME);
+//     // TestUseAlwaysModeledError(client, resource);
+//     // TestUseAlwaysMultipleErrors(client, resource);
+//     // TestUseAlwaysOpaqueError(client, resource);
+//   }
+
+  pub async fn TestCreateExtendableResource(
+    client: &Client,
+    name: &str
+  ) -> ExtendableResourceRef
+  {
+    client.create_extendable_resource()
+      .name(name)
+      .send()
+      .await
+      .unwrap()
+      .output()
+  }
+
+  pub async fn TestNoneUseExtendableResource(
+    client: &Client,
+    resource: ExtendableResourceRef,
+    name: &str
+  )
+  {
+    let dataInput = allNone();
+    let useOutput = client.use_extendable_resource()
+      .resource(resource)
+      .input(dataInput)
+      .send()
+      .await
+      .unwrap();
+
+    checkNone(name, useOutput.output());
+  }
+
+  pub async fn TestSomeUseExtendableResource(
+    client: &Client,
+    resource: ExtendableResourceRef,
+    name: &str
+  )
+  {
+    let dataInput = allSome();
+    let useOutput = client.use_extendable_resource()
+      .resource(resource)
+      .input(dataInput)
+      .send()
+      .await
+      .unwrap();
+
+    checkSome(name, useOutput.output());
+  }
+
 #[tokio::test]
 async fn TestNativeResource() {
     let resource: ExtendableResourceRef = DafnyFactory();
@@ -54,7 +140,7 @@ async fn TestSomeGetResourceData(resource: ExtendableResourceRef) {
         .borrow_mut()
         .get_extendable_resource_data(dataInput)
         .unwrap();
-    checkSome(DEFAULT_RESOURCE_NAME, dataOutput);
+    checkSome(DEFAULT_RESOURCE_NAME, &dataOutput);
 }
 
 async fn TestNoneGetResourceData(resource: ExtendableResourceRef) {
@@ -63,7 +149,7 @@ async fn TestNoneGetResourceData(resource: ExtendableResourceRef) {
         .borrow_mut()
         .get_extendable_resource_data(dataInput)
         .unwrap();
-    checkNone(DEFAULT_RESOURCE_NAME, dataOutput);
+    checkNone(DEFAULT_RESOURCE_NAME, &dataOutput);
 }
 
 pub fn client() -> Client {
@@ -75,7 +161,7 @@ pub fn allNone() -> GetExtendableResourceDataInput {
     GetExtendableResourceDataInput::builder().build().unwrap()
 }
 
-pub fn checkNone(name: &str, output: GetExtendableResourceDataOutput) {
+pub fn checkNone(name: &str, output: &GetExtendableResourceDataOutput) {
     assert_eq!(Some(name.to_string()), *output.string_value());
     assert_eq!(None, *output.blob_value());
     assert_eq!(None, output.boolean_value());
@@ -94,7 +180,7 @@ pub fn allSome() -> GetExtendableResourceDataInput {
         .unwrap()
 }
 
-pub fn checkSome(name: &str, output: GetExtendableResourceDataOutput) {
+pub fn checkSome(name: &str, output: &GetExtendableResourceDataOutput) {
     assert_eq!(
         Some("Some".to_string() + " " + name),
         *output.string_value()
