f590b0d2573d8e18138c89bae398a7cef13a4b73
Robin Salkeld <salkeldr@amazon.com>
2024-06-24 09:08:47 -0700

    Needed?

--- codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/smithy/dafny/codegen/CodegenCliTest.java
+++ codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/smithy/dafny/codegen/CodegenCliTest.java
@@ -77,7 +77,7 @@ class CodegenCliTest {
     private static final String[] PASSTHROUGH_ENVIRONMENT_VARIABLES = new String[] {
             "JAVA_HOME",
             "PATH",
-            "DOTNET_CLI_HOME",
+//            "DOTNET_CLI_HOME",
             "DAFNY_VERSION"
     };
 
