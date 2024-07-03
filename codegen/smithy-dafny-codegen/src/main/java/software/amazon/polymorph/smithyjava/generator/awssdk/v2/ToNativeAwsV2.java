2baebfbcb4b62ed0949c185610316725019a6dae
Tony Knapp <5892063+texastony@users.noreply.github.com>
2024-07-03 08:50:11 -0700

    fix(Java-SDK): handle generic service exceptions (#466)

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithyjava/generator/awssdk/v2/ToNativeAwsV2.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithyjava/generator/awssdk/v2/ToNativeAwsV2.java
@@ -175,6 +175,7 @@ public class ToNativeAwsV2 extends ToNative {
       .addMethods(convertRelevant)
       .addMethods(convertServiceErrors)
       .addMethod(modeledService(subject.serviceShape))
+      .addMethod(errorOpaque())
       .build();
   }
 
@@ -510,5 +511,85 @@ public class ToNativeAwsV2 extends ToNative {
       });
     method.addStatement("return $L.build()", VAR_BUILDER);
     return method.build();
+  }
+
+  protected MethodSpec errorOpaque() {
+    final String methodName = "Error";
+    final TypeName inputType = subject.dafnyNameResolver.classForOpaqueError();
+    final ClassName returnType = ClassName.get(RuntimeException.class);
+    return initializeMethodSpec(methodName, inputType, returnType)
+      .addComment("While the first two cases are logically identical,")
+      .addComment("there is a semantic distinction.")
+      .addComment(
+        "An un-modeled Service Error is different from a Java Heap Exhaustion error."
+      )
+      .addComment("In the future, Smithy-Dafny MAY allow for this distinction.")
+      .addComment(
+        "Which would allow Dafny developers to treat the two differently."
+      )
+      // If obj is an instance of the Service's Base Exception
+      .beginControlFlow(
+        "if ($L.$L instanceof $T)",
+        VAR_INPUT,
+        Dafny.datatypeDeconstructor("obj"),
+        subject.nativeNameResolver.baseErrorForService()
+      )
+      .addStatement(
+        "return ($T) $L.$L",
+        subject.nativeNameResolver.baseErrorForService(),
+        VAR_INPUT,
+        Dafny.datatypeDeconstructor("obj")
+      )
+      // If obj is ANY Exception
+      .nextControlFlow(
+        "else if ($L.$L instanceof $T)",
+        VAR_INPUT,
+        Dafny.datatypeDeconstructor("obj"),
+        Exception.class
+      )
+      .addStatement(
+        "return ($T) $L.$L",
+        RuntimeException.class,
+        VAR_INPUT,
+        Dafny.datatypeDeconstructor("obj")
+      )
+      // If String is set
+      .nextControlFlow(
+        "else if ($L.$L.$L())",
+        VAR_INPUT,
+        Dafny.datatypeDeconstructor("message"),
+        Dafny.datatypeConstructorIs("Some")
+      )
+      // if not null, stringify the object
+      .addStatement(
+        "final String suffix = $L.dtor_obj() != null ? String.format($S, $L.dtor_obj()) : $S;",
+        VAR_INPUT,
+        "  Unknown Object: %s",
+        VAR_INPUT,
+        ""
+      )
+      // Convert String from Dafny
+      .addStatement(
+        "final $T message = $L($L.$L.$L) + suffix",
+        String.class,
+        SIMPLE_CONVERSION_METHOD_FROM_SHAPE_TYPE
+          .get(ShapeType.STRING)
+          .asNormalReference(),
+        VAR_INPUT,
+        Dafny.datatypeDeconstructor("message"),
+        Dafny.datatypeDeconstructor("value")
+      )
+      .addStatement("return new $T(message)", RuntimeException.class)
+      .endControlFlow()
+      // If obj is not ANY exception and String is not set, Give Up with IllegalStateException
+      .addStatement(
+        "return new $T(String.format($S, $L))",
+        IllegalStateException.class,
+        "Unknown error thrown while calling " +
+        AwsSdkNativeV2.titleForService(subject.serviceShape) +
+        ". %s",
+        VAR_INPUT
+      )
+      .build();
   }
 }
