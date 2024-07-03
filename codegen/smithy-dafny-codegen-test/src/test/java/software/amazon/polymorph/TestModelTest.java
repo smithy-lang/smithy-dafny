95c244688431578c941bd2c554b321370241821b
Robin Salkeld <salkeldr@amazon.com>
2024-07-03 14:56:09 -0700

    more debugging

--- codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/TestModelTest.java
+++ codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/TestModelTest.java
@@ -129,5 +129,9 @@ public abstract class TestModelTest {
         "make command [" + args + "] failed (exit code: " + exitCode + "). Output:\n" + output
       );
     }
+
+    // TODO: debugging
+    System.out.println(args);
+    System.out.println(output);
   }
 }
