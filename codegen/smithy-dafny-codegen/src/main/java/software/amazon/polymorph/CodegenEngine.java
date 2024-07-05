132a7793eeaba670cd239454869930c9bc8679dd
Robin Salkeld <salkeldr@amazon.com>
2024-07-05 13:16:34 -0700

    formatting

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/CodegenEngine.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/CodegenEngine.java
@@ -606,7 +606,8 @@ public class CodegenEngine {
     parameters.put("stdLibPath", standardLibraryPath().toString());
 
     if (awsSdkStyle) {
-      final String clientClassName = ((AwsSdkDotNetNameResolver) resolver).implForServiceClient();
+      final String clientClassName =
+        ((AwsSdkDotNetNameResolver) resolver).implForServiceClient();
       parameters.put("sdkClientName", clientClassName);
 
       if (generationAspects.contains(GenerationAspect.PROJECT_FILES)) {
