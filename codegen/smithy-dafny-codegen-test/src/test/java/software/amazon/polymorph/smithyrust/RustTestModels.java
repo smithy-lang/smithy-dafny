7700281bd90cfee5882582038039b134b8db3df3
Robin Salkeld <salkeldr@amazon.com>
2024-07-11 21:12:07 -0700

    Enable test in CI

--- codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/smithyrust/RustTestModels.java
+++ codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/smithyrust/RustTestModels.java
@@ -40,7 +40,6 @@ class RustTestModels extends TestModelTest {
     DISABLED_TESTS.add("aws-sdks/glue");
     DISABLED_TESTS.add("aws-sdks/lakeformation");
     DISABLED_TESTS.add("aws-sdks/kms");
-    DISABLED_TESTS.add("aws-sdks/kms-lite");
     DISABLED_TESTS.add("aws-sdks/sqs");
     DISABLED_TESTS.add("aws-sdks/sqs-via-cli");
   }
