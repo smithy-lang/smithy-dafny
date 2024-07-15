56c552d0c81325d6017571c580e7b9a8fbea3aee
Robin Salkeld <salkeldr@amazon.com>
2024-07-14 17:20:25 -0700

    SimpleDouble

--- TestModels/SimpleTypes/SimpleDouble/runtimes/rust/src/client.rs
+++ TestModels/SimpleTypes/SimpleDouble/runtimes/rust/src/client.rs
@@ -4,7 +4,7 @@ use aws_smithy_types::error::operation::BuildError;
 
 #[derive(::std::clone::Clone, ::std::fmt::Debug)]
 pub struct Client {
-    pub(crate) dafny_client: ::dafny_runtime::Object<dyn ::simple_double_dafny::r#_simple_dtypes_dsmithydouble_dinternaldafny_dtypes::ISimpleTypesDoubleClient>
+    pub(crate) dafny_client: ::dafny_runtime::Object<dyn crate::implementation_from_dafny::r#_simple_dtypes_dsmithydouble_dinternaldafny_dtypes::ISimpleTypesDoubleClient>
 }
 
 impl Client {
@@ -14,12 +14,12 @@ impl Client {
         conf: crate::types::simple_double_config::SimpleDoubleConfig,
     ) -> Result<Self, BuildError> {
         let inner =
-            ::simple_double_dafny::_simple_dtypes_dsmithydouble_dinternaldafny::_default::SimpleDouble(
+            crate::implementation_from_dafny::_simple_dtypes_dsmithydouble_dinternaldafny::_default::SimpleDouble(
                 &crate::conversions::simple_double_config::_simple_double_config::to_dafny(conf),
             );
         if matches!(
             inner.as_ref(),
-            ::simple_double_dafny::_Wrappers_Compile::Result::Failure { .. }
+            crate::implementation_from_dafny::_Wrappers_Compile::Result::Failure { .. }
         ) {
             // TODO: convert error - the potential types are not modeled!
             return Err(BuildError::other(
--- TestModels/SimpleTypes/SimpleDouble/runtimes/rust/src/conversions/get_double/_get_double_input.rs
+++ TestModels/SimpleTypes/SimpleDouble/runtimes/rust/src/conversions/get_double/_get_double_input.rs
@@ -3,17 +3,17 @@
 pub fn to_dafny(
     value: crate::operation::get_double::GetDoubleInput,
 ) -> ::std::rc::Rc<
-    ::simple_double_dafny::r#_simple_dtypes_dsmithydouble_dinternaldafny_dtypes::GetDoubleInput,
+    crate::implementation_from_dafny::r#_simple_dtypes_dsmithydouble_dinternaldafny_dtypes::GetDoubleInput,
 > {
     let dafny_value = match value.value {
-        Some(v) => ::simple_double_dafny::_Wrappers_Compile::Option::Some {
+        Some(v) => crate::implementation_from_dafny::_Wrappers_Compile::Option::Some {
             value: ::dafny_runtime::Sequence::ArraySequence {
                 values: std::rc::Rc::new(f64::to_be_bytes(v).to_vec()),
             },
         },
-        None => ::simple_double_dafny::_Wrappers_Compile::Option::None {},
+        None => crate::implementation_from_dafny::_Wrappers_Compile::Option::None {},
     };
-    ::std::rc::Rc::new(::simple_double_dafny::r#_simple_dtypes_dsmithydouble_dinternaldafny_dtypes::GetDoubleInput::GetDoubleInput {
+    ::std::rc::Rc::new(crate::implementation_from_dafny::r#_simple_dtypes_dsmithydouble_dinternaldafny_dtypes::GetDoubleInput::GetDoubleInput {
     value: ::std::rc::Rc::new(dafny_value)
   })
 }
@@ -21,12 +21,12 @@ pub fn to_dafny(
 #[allow(dead_code)]
 pub fn from_dafny(
     dafny_value: ::std::rc::Rc<
-        ::simple_double_dafny::r#_simple_dtypes_dsmithydouble_dinternaldafny_dtypes::GetDoubleInput,
+        crate::implementation_from_dafny::r#_simple_dtypes_dsmithydouble_dinternaldafny_dtypes::GetDoubleInput,
     >,
 ) -> crate::operation::get_double::GetDoubleInput {
     let value = if matches!(
         dafny_value.value().as_ref(),
-        ::simple_double_dafny::_Wrappers_Compile::Option::Some { .. }
+        crate::implementation_from_dafny::_Wrappers_Compile::Option::Some { .. }
     ) {
         let my_vec = dafny_runtime::dafny_runtime_conversions::dafny_sequence_to_vec(
             &dafny_value.value().Extract(),
@@ -37,7 +37,7 @@ pub fn from_dafny(
         ))
     } else if matches!(
         dafny_value.value().as_ref(),
-        ::simple_double_dafny::_Wrappers_Compile::Option::None { .. }
+        crate::implementation_from_dafny::_Wrappers_Compile::Option::None { .. }
     ) {
         None
     } else {
