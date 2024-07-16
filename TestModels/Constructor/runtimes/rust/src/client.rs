e4a3b2d174e46faff24189b67da885420e9a709a
Andrew Banchich <banchich@amazon.com>
2024-07-15 22:51:05 -0400

    remove commented code

--- TestModels/Constructor/runtimes/rust/src/client.rs
+++ TestModels/Constructor/runtimes/rust/src/client.rs
@@ -11,11 +11,6 @@ impl Client {
     /// Creates a new client from the service [`Config`](crate::Config).
     #[track_caller]
     pub fn from_conf(conf: crate::Config) -> Result<Self, BuildError> {
-        // let inner =
-        //     ::simple_integer_dafny::_simple_dtypes_dinteger_dinternaldafny::_default::SimpleInteger(
-        //         &crate::conversions::simple_integer_config::_simple_integer_config::to_dafny(conf),
-        //     );
-
         let inner =
             ::constructor_dafny::r#_simple_dconstructor_dinternaldafny::_default::SimpleConstructor(
                 &crate::conversions::simple_constructor_config::_simple_constructor_config::to_dafny(conf),
