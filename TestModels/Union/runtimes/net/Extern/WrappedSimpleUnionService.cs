0b6f10451912b3f164af48566dfc64f6ccf7ebfb
rishav-karanjit <karanjitrishav4@gmail.com>
2024-07-23 12:00:21 -0700

    Pushing runtime to debug

--- TestModels/Union/runtimes/net/Extern/WrappedSimpleUnionService.cs
+++ TestModels/Union/runtimes/net/Extern/WrappedSimpleUnionService.cs
@@ -6,8 +6,10 @@ using Simple.Union.Wrapped;
 using TypeConversion = Simple.Union.TypeConversion;
 namespace simple.union.internaldafny.wrapped
 {
-    public partial class __default {
-        public static _IResult<types.ISimpleUnionClient, types._IError> WrappedSimpleUnion(types._ISimpleUnionConfig config) {
+    public partial class __default
+    {
+        public static _IResult<types.ISimpleUnionClient, types._IError> WrappedSimpleUnion(types._ISimpleUnionConfig config)
+        {
             var wrappedConfig = TypeConversion.FromDafny_N6_simple__N5_union__S17_SimpleUnionConfig(config);
             var impl = new SimpleUnion(wrappedConfig);
             var wrappedClient = new SimpleUnionShim(impl);
