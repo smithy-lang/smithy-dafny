50e43e22c8dc8ee76a8eb047ff1e23710d999966
rishav-karanjit <karanjitrishav4@gmail.com>
2024-07-23 12:35:34 -0700

    Revert "Push some more runtime"
    This reverts commit ac04b6f9bfcf3798281f1806636cf1b45bca8089.

--- TestModels/Constraints/runtimes/java/src/main/dafny-generated/simple/constraints/internaldafny/types/ISimpleConstraintsClient.java
+++
@@ -1,9 +0,0 @@
-// Interface ISimpleConstraintsClient
-// Dafny trait ISimpleConstraintsClient compiled into Java
-package simple.constraints.internaldafny.types;
-
-
-@SuppressWarnings({"unchecked", "deprecation"})
-public interface ISimpleConstraintsClient {
-  public Wrappers_Compile.Result<GetConstraintsOutput, Error> GetConstraints(GetConstraintsInput input);
-}
