e4302163346187ca18858a81dc4816fbbe751e54
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-07-25 12:52:01 -0700

    sync

--- TestModels/Constraints/runtimes/java/src/main/java/simple/constraints/internaldafny/types/__default.java
+++ TestModels/Constraints/runtimes/java/src/main/java/simple/constraints/internaldafny/types/__default.java
@@ -1,5 +1,5 @@
 // Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-package simple_constraints_internaldafny_types;
+package simple.constraints.internaldafny.types;
 
 public class __default extends _ExternBase___default {}
