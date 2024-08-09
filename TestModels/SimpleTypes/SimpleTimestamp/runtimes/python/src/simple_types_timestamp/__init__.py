5ed12479003953e34c474c16a45aaa3ec5ce14ea
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-08-09 10:45:12 -0700

    no simple timestamp

--- TestModels/SimpleTypes/SimpleTimestamp/runtimes/python/src/simple_types_timestamp/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-# Initialize generated Dafny
-from .internaldafny.generated import module_
