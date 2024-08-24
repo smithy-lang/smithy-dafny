9ba46ec192303def215a04a310dcab924bbf754b
Alex Chew <alexchew@amazon.com>
2024-08-23 18:19:29 -0700

    codegen Rust runtime for SimpleString

--- TestModels/SimpleTypes/SimpleString/runtimes/rust/src/operation/get_string_utf8_known_value/_get_string_utf8_input.rs
+++ TestModels/SimpleTypes/SimpleString/runtimes/rust/src/operation/get_string_utf8_known_value/_get_string_utf8_input.rs
@@ -4,32 +4,32 @@
 #[allow(missing_docs)] // documentation missing in model
 #[non_exhaustive]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
-pub struct GetStringOutput {
+pub struct GetStringUTF8Input {
     #[allow(missing_docs)] // documentation missing in model
 pub value: ::std::option::Option<::std::string::String>,
 }
-impl GetStringOutput {
+impl GetStringUTF8Input {
     #[allow(missing_docs)] // documentation missing in model
 pub fn value(&self) -> ::std::option::Option<::std::string::String> {
     self.value.clone()
 }
 }
-impl GetStringOutput {
-    /// Creates a new builder-style object to manufacture [`GetStringOutput`](crate::operation::operation::GetStringOutput).
-    pub fn builder() -> crate::operation::get_string::builders::GetStringOutputBuilder {
-        crate::operation::get_string::builders::GetStringOutputBuilder::default()
+impl GetStringUTF8Input {
+    /// Creates a new builder-style object to manufacture [`GetStringUTF8Input`](crate::operation::operation::GetStringUTF8Input).
+    pub fn builder() -> crate::operation::get_string_utf8_known_value::builders::GetStringUTF8InputBuilder {
+        crate::operation::get_string_utf8_known_value::builders::GetStringUTF8InputBuilder::default()
     }
 }
 
-/// A builder for [`GetStringOutput`](crate::operation::operation::GetStringOutput).
+/// A builder for [`GetStringUTF8Input`](crate::operation::operation::GetStringUTF8Input).
 #[non_exhaustive]
 #[derive(
     ::std::clone::Clone, ::std::cmp::PartialEq, ::std::default::Default, ::std::fmt::Debug,
 )]
-pub struct GetStringOutputBuilder {
+pub struct GetStringUTF8InputBuilder {
     pub(crate) value: ::std::option::Option<::std::string::String>,
 }
-impl GetStringOutputBuilder {
+impl GetStringUTF8InputBuilder {
     #[allow(missing_docs)] // documentation missing in model
 pub fn value(mut self, input: impl ::std::convert::Into<::std::string::String>) -> Self {
     self.value = ::std::option::Option::Some(input.into());
@@ -44,14 +44,14 @@ pub fn set_value(mut self, input: ::std::option::Option<::std::string::String>) 
 pub fn get_value(&self) -> &::std::option::Option<::std::string::String> {
     &self.value
 }
-    /// Consumes the builder and constructs a [`GetStringOutput`](crate::operation::operation::GetStringOutput).
+    /// Consumes the builder and constructs a [`GetStringUTF8Input`](crate::operation::operation::GetStringUTF8Input).
     pub fn build(
         self,
     ) -> ::std::result::Result<
-        crate::operation::get_string::GetStringOutput,
+        crate::operation::get_string_utf8_known_value::GetStringUTF8Input,
         ::aws_smithy_types::error::operation::BuildError,
     > {
-        ::std::result::Result::Ok(crate::operation::get_string::GetStringOutput {
+        ::std::result::Result::Ok(crate::operation::get_string_utf8_known_value::GetStringUTF8Input {
             value: self.value,
         })
     }
