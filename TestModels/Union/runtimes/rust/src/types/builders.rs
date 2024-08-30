efd3766a751d5325110639bf88c09dcb2ef5e5b4
Alex Chew <alexchew@amazon.com>
2024-08-29 23:33:05 -0700

    regenerate Rust runtime for Union model

--- TestModels/Union/runtimes/rust/src/types/builders.rs
+++ TestModels/Union/runtimes/rust/src/types/builders.rs
@@ -1,8 +1,3 @@
 // Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 // Do not modify this file. This file is machine generated, and any changes to it will be overwritten.
-/// Types for the `GetKnownValueUnion` operation.
-pub mod get_known_value_union;
-
-/// Types for the `GetUnion` operation.
-pub mod get_union;
