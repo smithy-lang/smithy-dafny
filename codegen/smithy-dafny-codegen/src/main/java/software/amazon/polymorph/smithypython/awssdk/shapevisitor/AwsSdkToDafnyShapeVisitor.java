78fb73b8f446a9c0ba9143a1f0f8c1c6e76c1490
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-09-04 15:28:03 -0700

    Update codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithypython/awssdk/shapevisitor/AwsSdkToDafnyShapeVisitor.java

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithypython/awssdk/shapevisitor/AwsSdkToDafnyShapeVisitor.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithypython/awssdk/shapevisitor/AwsSdkToDafnyShapeVisitor.java
@@ -206,6 +206,7 @@ public class AwsSdkToDafnyShapeVisitor extends ShapeVisitor.Default<String> {
       return enumShape(EnumShape.fromStringShape(shape).get());
     }
 
+    // Convert native Python string to Dafny Seq of UTF-16 characters
     return "Seq(''.join([chr(int.from_bytes(pair, 'big')) for pair in zip(*[iter(%1$s.encode('utf-16-be'))]*2)]))".formatted(
         dataSource
       );
