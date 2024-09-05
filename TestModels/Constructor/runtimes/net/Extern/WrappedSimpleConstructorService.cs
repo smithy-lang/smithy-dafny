383274252bad28fd941d7fcbe1c5dd623a34f852
Andrew Jewell <107044381+ajewellamz@users.noreply.github.com>
2024-09-05 14:57:53 -0400

    feat: real code generation for Constructor in Rust (#562)

--- TestModels/Constructor/runtimes/net/Extern/WrappedSimpleConstructorService.cs
+++ TestModels/Constructor/runtimes/net/Extern/WrappedSimpleConstructorService.cs
@@ -3,11 +3,13 @@
 using Wrappers_Compile;
 using Simple.Constructor;
 using Simple.Constructor.Wrapped;
-using TypeConversion = Simple.Constructor.TypeConversion ;
+using TypeConversion = Simple.Constructor.TypeConversion;
 namespace simple.constructor.internaldafny.wrapped
 {
-    public partial class __default {
-        public static _IResult<types.ISimpleConstructorClient, types._IError> WrappedSimpleConstructor(types._ISimpleConstructorConfig config) {
+    public partial class __default
+    {
+        public static _IResult<types.ISimpleConstructorClient, types._IError> WrappedSimpleConstructor(types._ISimpleConstructorConfig config)
+        {
             var wrappedConfig = TypeConversion.FromDafny_N6_simple__N11_constructor__S23_SimpleConstructorConfig(config);
             var impl = new SimpleConstructor(wrappedConfig);
             var wrappedClient = new SimpleConstructorShim(impl);
