a9afaacd31f02cf5cfb0a636116ff2ce103857ff
Andrew Jewell <107044381+ajewellamz@users.noreply.github.com>
2024-09-05 14:57:53 -0400

    feat: real code generation for Constructor in Rust (#562)

--- TestModels/Aggregate/runtimes/rust/src/types/builders.rs
+++
@@ -1,8 +0,0 @@
-// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-// Do not modify this file. This file is machine generated, and any changes to it will be overwritten.
-pub use crate::types::_nested_structure::NestedStructureBuilder;
-
-pub use crate::types::_string_structure::StringStructureBuilder;
-
-pub use crate::types::_structure_list_element::StructureListElementBuilder;
