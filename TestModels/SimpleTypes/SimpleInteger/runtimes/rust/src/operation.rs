a9fc53aa95e5ad0570a930c2a077587c98cec8a7
Andy Jewell <ajewell@amazon.com>
2024-09-05 11:44:08 -0400

    fix: remove generated code from simple types

--- TestModels/SimpleTypes/SimpleInteger/runtimes/rust/src/operation.rs
+++
@@ -1,8 +0,0 @@
-// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-// Do not modify this file. This file is machine generated, and any changes to it will be overwritten.
-/// Types for the `GetInteger` operation.
-pub mod get_integer;
-
-/// Types for the `GetIntegerKnownValueTest` operation.
-pub mod get_integer_known_value_test;
