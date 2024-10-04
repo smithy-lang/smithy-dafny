9b82090fe9c43327754b9d5dd4ba9d7d2b0f1d75
Andy Jewell <ajewell@amazon.com>
2024-10-04 10:37:15 -0400

    m

--- TestModels/Extern/runtimes/python/src/simple_dafnyextern/internaldafny/extern/SimpleExternImpl.py
+++ TestModels/Extern/runtimes/python/src/simple_dafnyextern/internaldafny/extern/SimpleExternImpl.py
@@ -18,7 +18,7 @@ def GetExtern(config, input):
 @staticmethod
 def ExternMustError(config, input):
   exception = Exception(input)
-  return Wrappers.Result_Failure(SimpleDafnyExternTypes.Error_Opaque(exception))
+  return Wrappers.Result_Failure(SimpleDafnyExternTypes.Error_Opaque(exception, repr(input)))
 
 default__.GetExtern = GetExtern
 default__.ExternMustError = ExternMustError
