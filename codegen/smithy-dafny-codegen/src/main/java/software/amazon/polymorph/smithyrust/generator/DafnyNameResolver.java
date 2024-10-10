5fe16bed16582e8d3aea1b381b7255f3ffe0a30a
Robin Salkeld <salkeldr@amazon.com>
2024-10-10 13:03:22 -0700

    Revert "Revert _None escaping"
    This reverts commit 7ed94bd07661748d958dcb6e260f461e5054aae2.

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithyrust/generator/DafnyNameResolver.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithyrust/generator/DafnyNameResolver.java
@@ -3,6 +3,9 @@ package software.amazon.polymorph.smithyrust.generator;
 public class DafnyNameResolver {
 
   public static String escapeName(String name) {
+    if (name.equals("None")) {
+      return "_None";
+    }
     return name;
   }
 }
