4c14301f5a24bb760ba68e4739e9c64c88e885d8
Robin Salkeld <salkeldr@amazon.com>
2024-10-09 17:13:16 -0700

    Remove MultipleModels generated files

--- TestModels/MultipleModels/runtimes/rust/src/deps/simple_multiplemodels_dependencyproject/types/builders.rs
+++
@@ -1,6 +0,0 @@
-// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-// Do not modify this file. This file is machine generated, and any changes to it will be overwritten.
-pub use crate::deps::simple_multiplemodels_dependencyproject::types::_some_dependency_operation_input::SomeDependencyOperationInputBuilder;
-
-pub use crate::deps::simple_multiplemodels_dependencyproject::types::_some_dependency_operation_output::SomeDependencyOperationOutputBuilder;
--- TestModels/MultipleModels/runtimes/rust/src/operation.rs
+++
@@ -1,8 +0,0 @@
-// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-// Do not modify this file. This file is machine generated, and any changes to it will be overwritten.
-/// Types for the `SomePrimaryOperation` operation.
-pub mod some_primary_operation;
-
-/// Types for the `SomeDependencyOperation` operation.
-pub mod some_dependency_operation;
