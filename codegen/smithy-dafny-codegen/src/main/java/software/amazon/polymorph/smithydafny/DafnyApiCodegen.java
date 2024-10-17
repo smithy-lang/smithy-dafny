32ec75b08f8882b40a1dbea42e23c2b084898431
seebees <ryanemer@amazon.com>
2024-10-17 16:08:23 -0700

    formating

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithydafny/DafnyApiCodegen.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithydafny/DafnyApiCodegen.java
@@ -1487,13 +1487,23 @@ public class DafnyApiCodegen {
                 ),
                 isOutput
                   ? TokenTree.of(
-                    Token.of("&& fresh(%1$s.%2$s)".formatted(varName, s.getMemberName())),
+                    Token.of(
+                      "&& fresh(%1$s.%2$s)".formatted(
+                          varName,
+                          s.getMemberName()
+                        )
+                    ),
                     Token
                       .of("&& fresh")
                       .append(
                         TokenTree
                           .of(
-                            Token.of("%1$s.%2$s.Modifies".formatted(varName, s.getMemberName())),
+                            Token.of(
+                              "%1$s.%2$s.Modifies".formatted(
+                                  varName,
+                                  s.getMemberName()
+                                )
+                            ),
                             removeInputs
                           )
                           .parenthesized()
