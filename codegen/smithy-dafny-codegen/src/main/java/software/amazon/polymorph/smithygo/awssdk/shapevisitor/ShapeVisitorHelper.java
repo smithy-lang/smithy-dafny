750ea96c1595c87cff83d1fd45c636eb1195d27c
rishav-karanjit <karanjitrishav4@gmail.com>
2024-10-17 14:36:42 -0700

    Remove redundant var

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithygo/awssdk/shapevisitor/ShapeVisitorHelper.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithygo/awssdk/shapevisitor/ShapeVisitorHelper.java
@@ -70,8 +70,7 @@ public class ShapeVisitorHelper {
       pointerShapesToNative.put(memberShape, isPointable);
     }
     final String funcName = funcNameGenerator(memberShape, "FromDafny");
-    final String nextVisitorFunction = funcName.concat("(").concat(dataSource).concat(")");
-    return nextVisitorFunction;
+    return(funcName.concat("(").concat(dataSource).concat(")"));
   }
 
   public static String toDafnyShapeVisitorWriter(
@@ -117,7 +116,6 @@ public class ShapeVisitorHelper {
       );
     }
     final String funcName = funcNameGenerator(memberShape, "ToDafny");
-    final String nextVisitorFunction = funcName.concat("(").concat(dataSource).concat(")");
-    return nextVisitorFunction;
+    return(funcName.concat("(").concat(dataSource).concat(")"));
   }
 }
