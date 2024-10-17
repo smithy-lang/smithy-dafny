f383a06d521e6300d0795bf3be88bca82f955733
seebees <ryanemer@amazon.com>
2024-10-16 21:06:30 -0700

    Add Validator

---
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/traits/MutableLocalStateTraitValidator.java
@@ -0,0 +1,72 @@
+package software.amazon.polymorph.traits;
+
+import java.util.ArrayList;
+import java.util.List;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.knowledge.OperationIndex;
+import software.amazon.smithy.model.selector.Selector;
+import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.StructureShape;
+import software.amazon.smithy.model.validation.AbstractValidator;
+import software.amazon.smithy.model.validation.ValidationEvent;
+import software.amazon.polymorph.traits.MutableLocalStateTrait;
+
+public class MutableLocalStateTraitValidator extends AbstractValidator {
+  @Override
+  public List<ValidationEvent> validate(Model model) {
+    List<ValidationEvent> events = new ArrayList<>();
+    for (Shape shape : model.getShapesWithTrait(MutableLocalStateTrait.class)) {
+
+      // Generate danger for any use of this trait.
+      // See the danger message for details.
+      events.add(
+        danger(
+          shape,
+          """
+            aws.polymorph#mutableLocalState can allow callers to introduce soundness issues.
+                    
+            At this time to support dynamic mutable state smithy-dafny creates a seperated class by using an {:axiom}.
+            The idea for these seperated classes is that the resource has access to an internal modifies set.
+            This is sometimes referred to as an Repr set.
+            The resources internal operations are allowed to modify this InternalModifies,
+            but the public operations are not.
+            A Dafny `assume {:axiom}` is used to create this separation between the InternalModifies and the Modifies sets.
+                    
+            Smithy-dafny operations that return these resources will always return the `trait`
+            which does not have any concrete implementation of state.
+            However, a clever caller could cast this trait into its concrete class.
+            Such a caller now has access to observe the internal state of the resource.
+                    
+            Dafny will allow such a caller to prove that this state is unchanged.
+            Because the called operations do not contain these elements inside their `modifies` clause.
+            But these elements would indeed be modified.
+            Thus Dafny could prove false.
+                    
+            ```dafny
+                    
+            var Output(resource) :- expect client.GetResource();
+                    
+            expect resource is ConcreteResource;
+            var concreteResource := resource as ConcreteResource;
+            
+            // Get a copy of state before state changes
+            var state := concreteResource.state;
+            
+            var output2 :- expect concreteResource.ChangeState();
+            
+            // State will have changed, because we called for a state change.
+            // however, Dafny can prove that state MUST NOT have changed,
+            // because concreteResource.ChangeState modifies == concreteResource.Modifies
+            // and `this !in concreteResource.Modifies`
+            assert state == concreteResource.state;
+            expect state != concreteResource.state;
+            ```
+            """
+        )
+      );
+    }
+
+    return events;
+  }
+
+}
