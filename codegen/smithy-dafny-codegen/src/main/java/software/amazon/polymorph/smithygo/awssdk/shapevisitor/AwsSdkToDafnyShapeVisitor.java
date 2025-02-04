0c76cda134b3b0d0abc92127e0574be5ac3a4e1e
rishav-karanjit <karanjitrishav4@gmail.com>
2025-02-04 10:41:39 -0800

    formatting

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithygo/awssdk/shapevisitor/AwsSdkToDafnyShapeVisitor.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithygo/awssdk/shapevisitor/AwsSdkToDafnyShapeVisitor.java
@@ -245,7 +245,8 @@ public class AwsSdkToDafnyShapeVisitor extends ShapeVisitor.Default<String> {
       someWrapIfRequired = "Wrappers.Companion_Option_.Create_Some_(%s)";
       returnType = "Wrappers.Option";
     }
-    var nilCheck = "if %s == nil {return %s}".formatted(dataSource, nilWrapIfRequired);
+    var nilCheck =
+      "if %s == nil {return %s}".formatted(dataSource, nilWrapIfRequired);
     typeConversionMethodBuilder.append(
       """
       func () %s {
