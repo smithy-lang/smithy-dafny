46b4ad7fb07394408c05e8239bb779f1dec3c171
seebees <ryanemer@amazon.com>
2025-03-11 16:47:39 -0700

    last one?

--- codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/smithyjava/JavaTestModels.java
+++ codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/smithyjava/JavaTestModels.java
@@ -45,7 +45,7 @@ class JavaTestModels extends TestModelTest {
 
 
     DISABLED_TESTS.add("Dependencies");
-    
+
 
 
 
@@ -125,7 +125,7 @@ class JavaTestModels extends TestModelTest {
     DISABLED_TESTS.add("aws-sdks/kms-lite");
 //    DISABLED_TESTS.add("aws-sdks/kmsv2");
 //    DISABLED_TESTS.add("aws-sdks/lakeformation");
-//    DISABLED_TESTS.add("aws-sdks/s3");
+    DISABLED_TESTS.add("aws-sdks/s3");
     DISABLED_TESTS.add("aws-sdks/sqs");
     DISABLED_TESTS.add("aws-sdks/sqs-via-cli");
 //    DISABLED_TESTS.add("dafny-dependencies/StandardLibrary");
