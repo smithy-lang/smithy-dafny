a98b9bb47527fdd9980735054482aa07b3814a06
Shubham Chaturvedi <scchatur@amazon.com>
2025-03-11 16:03:11 -0700

    fix(Go): Std Lib UTF16 decode replacementchar handling

--- TestModels/dafny-dependencies/StandardLibrary/runtimes/go/ImplementationFromDafny-go/UTF8/externs.go
+++ TestModels/dafny-dependencies/StandardLibrary/runtimes/go/ImplementationFromDafny-go/UTF8/externs.go
@@ -2,11 +2,22 @@ package UTF8
 
 import (
 	"fmt"
+	"math"
 	"unicode/utf16"
 	"unicode/utf8"
 
 	"github.com/aws/aws-cryptographic-material-providers-library/releases/go/smithy-dafny-standard-library/Wrappers"
 	"github.com/dafny-lang/DafnyRuntimeGo/v4/dafny"
+)
+
+// The following constants are copied from the Go utf16 lib and are used
+// to check the validity of the utf16 surrogate pairs.
+const (
+	// 0xd800-0xdc00 encodes the high 10 bits of a pair.
+	// 0xdc00-0xe000 encodes the low 10 bits of a pair.
+	surr1 = 0xd800
+	surr2 = 0xdc00
+	surr3 = 0xe000
 )
 
 //IMP: The below extern implementations are only compatible
@@ -30,13 +41,9 @@ func Decode(utf8EncodedDafnySeq dafny.Sequence) Wrappers.Result {
 // Anything we receive here is supposed to be utf16 encoded Go rune
 // since this extern is for unicode-char:false.
 func Encode(utf16EncodedDafnySeq dafny.Sequence) Wrappers.Result {
-	encodedUtf16 := utf16EncodedDafnySeqToUint16(utf16EncodedDafnySeq)
-	decodedUtf16 := utf16.Decode(encodedUtf16)
-	var utf8EncodedBytes []byte
-	for _, r := range decodedUtf16 {
-		buf := make([]byte, utf8.RuneLen(r))
-		n := utf8.EncodeRune(buf, r)
-		utf8EncodedBytes = append(utf8EncodedBytes, buf[:n]...)
+	utf8EncodedBytes, err := decodeUtf16(utf16EncodedDafnySeq)
+	if err != nil {
+		return Wrappers.Companion_Result_.Create_Failure_(dafny.SeqOfString(err.Error()))
 	}
 	return Wrappers.Companion_Result_.Create_Success_(dafny.SeqOfBytes(utf8EncodedBytes))
 }
@@ -55,14 +62,61 @@ func DecodeFromNativeGoByteArray(utf8EncodedByteArray []byte) (dafny.Sequence, e
 	return dafny.SeqOfChars(dafnyCharArray...), nil
 }
 
-func utf16EncodedDafnySeqToUint16(seq dafny.Sequence) []uint16 {
-	var r []uint16
+// decode appends to buf the Unicode code point sequence represented
+// by the UTF-16 encoding seq, then encode the code point as utf8 and return the utf8 buffer
+func decodeUtf16(seq dafny.Sequence) ([]byte, error) {
+	utf8EncodedBytes := []byte{}
+
 	for i := dafny.Iterate(seq); ; {
-		val, ok := i()
-		if !ok {
-			return r
+		firstVal, firstValExists := i()
+		if !firstValExists {
+			// Iterator has finished, return the buffer
+			return utf8EncodedBytes, nil
 		} else {
-			r = append(r, uint16(val.(dafny.Char)))
+			var ar rune
+
+			// We should be able to rely on dafny that anything inside the seq is utf16 encoded
+			// with unicode-char: false. But given the Long Psi issue, it's better to be safe.
+			// First check if it's a dafny.Char type, then check if it's within the limits of uint16.
+			firstChar, firstValIsAChar := firstVal.(dafny.Char)
+			if !firstValIsAChar || firstChar > math.MaxUint16 || firstChar < 0 {
+				return nil, fmt.Errorf("invalid utf16 encoded sequence: %v", seq)
+			}
+
+			// Downcast to uint16
+			switch r1 := uint16(firstChar); {
+
+			case r1 < surr1, surr3 <= r1:
+				// normal rune
+				ar = rune(r1)
+
+			case utf16.IsSurrogate(rune(r1)):
+				// If firstVal is surrogate, then we need the secondVal to construct the pair
+				secondVal, ok := i()
+
+				// Same sanity check as line 84
+				secondChar, secondValIsAChar := secondVal.(dafny.Char)
+				if !ok || !secondValIsAChar || secondChar > math.MaxUint16 || secondChar < 0 {
+					return nil, fmt.Errorf("invalid utf16 encoded sequence: %v", seq)
+				}
+
+				// Check if the secondVal is within the valid low surrogate range
+				if surr2 <= uint16(secondChar) && uint16(secondChar) < surr3 {
+					// valid surrogate sequence
+					ar = utf16.DecodeRune(rune(r1), rune(uint16(secondChar)))
+				} else {
+					return nil, fmt.Errorf("invalid utf16 encoded sequence: %v", seq)
+				}
+			default:
+				return nil, fmt.Errorf("invalid utf16 encoded sequence: %v", seq)
+			}
+
+			// Create the buffer (upto 4 bytes) to hold the utf8 rune
+			buf := make([]byte, utf8.RuneLen(ar))
+			n := utf8.EncodeRune(buf, ar)
+
+			// Append to the result
+			utf8EncodedBytes = append(utf8EncodedBytes, buf[:n]...)
 		}
 	}
 }
