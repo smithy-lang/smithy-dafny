02fbab9ac12421bb85f7f3718fe2c4cc1b735212
seebees <ryanemer@amazon.com>
2025-03-17 13:18:50 -0700

    format

--- codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/smithyjava/JavaTestModels.java
+++ codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/smithyjava/JavaTestModels.java
@@ -20,7 +20,6 @@ class JavaTestModels extends TestModelTest {
   private static final Set<String> DISABLED_TESTS = new HashSet<>();
 
   static {
-    
     DISABLED_TESTS.add("Aggregate");
 
     DISABLED_TESTS.add("Dependencies"); // Smithy-Dafny Error
@@ -30,7 +29,7 @@ class JavaTestModels extends TestModelTest {
     DISABLED_TESTS.add("LanguageSpecificLogic"); // Smithy-Dafny Error
     DISABLED_TESTS.add("Positional"); // Smithy-Dafny Error
     DISABLED_TESTS.add("Refinement"); // Smithy-Dafny Error
-    DISABLED_TESTS.add("SimpleTypes/SimpleBoolean");  // Need runtime
+    DISABLED_TESTS.add("SimpleTypes/SimpleBoolean"); // Need runtime
     DISABLED_TESTS.add("SimpleTypes/SimpleDouble"); // Error in generated type conversion
     DISABLED_TESTS.add("SimpleTypes/SimpleTimestamp"); // Need to add for Java
 
@@ -39,46 +38,45 @@ class JavaTestModels extends TestModelTest {
     DISABLED_TESTS.add("aws-sdks/sqs"); // Not written yet
     DISABLED_TESTS.add("aws-sdks/sqs-via-cli"); // Not written yet
 
-    DISABLED_TESTS.add("AggregateReferences");  // Not supported yet
-    DISABLED_TESTS.add("RecursiveShape");  // Not supported yet
+    DISABLED_TESTS.add("AggregateReferences"); // Not supported yet
+    DISABLED_TESTS.add("RecursiveShape"); // Not supported yet
     DISABLED_TESTS.add("SimpleTypes/BigDecimal"); // Not supported yet
     DISABLED_TESTS.add("SimpleTypes/BigInteger"); // Not supported yet
     DISABLED_TESTS.add("SimpleTypes/SimpleByte"); // Not supported yet
     DISABLED_TESTS.add("SimpleTypes/SimpleDocument"); // Not supported yet
-    DISABLED_TESTS.add("SimpleTypes/SimpleFloat");  // Not supported yet
-    DISABLED_TESTS.add("SimpleTypes/SimpleShort");  // Not supported yet
-    DISABLED_TESTS.add("Streaming");  // Not supported yet
-
-//    These are commented out because they should work
-//    They are left here because it can be useful
-//    to have these here so that it is easy to only run a single test locally.
-//    DISABLED_TESTS.add("CallingAWSSDKFromLocalService"); // These work
-//    DISABLED_TESTS.add("CodegenPatches"); // These work
-//    DISABLED_TESTS.add("Constraints"); // These work
-//    DISABLED_TESTS.add("Constructor"); // These work
-//    DISABLED_TESTS.add("Documentation"); // These work
-//    DISABLED_TESTS.add("Errors"); // These work
-//    DISABLED_TESTS.add("Extendable"); // These work
-//    DISABLED_TESTS.add("LocalService"); // These work
-//    DISABLED_TESTS.add("MultipleModels"); // These work
-//    DISABLED_TESTS.add("OrphanedShapes"); // These work
-//    DISABLED_TESTS.add("Resource"); // These work
-//    DISABLED_TESTS.add("SQSExtended"); // These work
-//    DISABLED_TESTS.add("SimpleTypes/SimpleBlob"); // These work
-//    DISABLED_TESTS.add("SimpleTypes/SimpleEnum"); // These work
-//    DISABLED_TESTS.add("SimpleTypes/SimpleEnumV2"); // These work
-//    DISABLED_TESTS.add("SimpleTypes/SimpleInteger"); // These work
-//    DISABLED_TESTS.add("SimpleTypes/SimpleLong"); // These work
-//    DISABLED_TESTS.add("SimpleTypes/SimpleString"); // These work
-//    DISABLED_TESTS.add("Union"); // These work
-//    DISABLED_TESTS.add("aws-sdks/ddb"); // These work
-//    DISABLED_TESTS.add("aws-sdks/ddb-lite"); // These work
-//    DISABLED_TESTS.add("aws-sdks/ddbv2"); // These work
-//    DISABLED_TESTS.add("aws-sdks/glue"); // These work
-//    DISABLED_TESTS.add("aws-sdks/kms"); // These work
-//    DISABLED_TESTS.add("aws-sdks/kmsv2"); // These work
-//    DISABLED_TESTS.add("aws-sdks/lakeformation"); // These work
-//    DISABLED_TESTS.add("dafny-dependencies/StandardLibrary"); // These work
+    DISABLED_TESTS.add("SimpleTypes/SimpleFloat"); // Not supported yet
+    DISABLED_TESTS.add("SimpleTypes/SimpleShort"); // Not supported yet
+    DISABLED_TESTS.add("Streaming"); // Not supported yet
+    //    These are commented out because they should work
+    //    They are left here because it can be useful
+    //    to have these here so that it is easy to only run a single test locally.
+    //    DISABLED_TESTS.add("CallingAWSSDKFromLocalService"); // These work
+    //    DISABLED_TESTS.add("CodegenPatches"); // These work
+    //    DISABLED_TESTS.add("Constraints"); // These work
+    //    DISABLED_TESTS.add("Constructor"); // These work
+    //    DISABLED_TESTS.add("Documentation"); // These work
+    //    DISABLED_TESTS.add("Errors"); // These work
+    //    DISABLED_TESTS.add("Extendable"); // These work
+    //    DISABLED_TESTS.add("LocalService"); // These work
+    //    DISABLED_TESTS.add("MultipleModels"); // These work
+    //    DISABLED_TESTS.add("OrphanedShapes"); // These work
+    //    DISABLED_TESTS.add("Resource"); // These work
+    //    DISABLED_TESTS.add("SQSExtended"); // These work
+    //    DISABLED_TESTS.add("SimpleTypes/SimpleBlob"); // These work
+    //    DISABLED_TESTS.add("SimpleTypes/SimpleEnum"); // These work
+    //    DISABLED_TESTS.add("SimpleTypes/SimpleEnumV2"); // These work
+    //    DISABLED_TESTS.add("SimpleTypes/SimpleInteger"); // These work
+    //    DISABLED_TESTS.add("SimpleTypes/SimpleLong"); // These work
+    //    DISABLED_TESTS.add("SimpleTypes/SimpleString"); // These work
+    //    DISABLED_TESTS.add("Union"); // These work
+    //    DISABLED_TESTS.add("aws-sdks/ddb"); // These work
+    //    DISABLED_TESTS.add("aws-sdks/ddb-lite"); // These work
+    //    DISABLED_TESTS.add("aws-sdks/ddbv2"); // These work
+    //    DISABLED_TESTS.add("aws-sdks/glue"); // These work
+    //    DISABLED_TESTS.add("aws-sdks/kms"); // These work
+    //    DISABLED_TESTS.add("aws-sdks/kmsv2"); // These work
+    //    DISABLED_TESTS.add("aws-sdks/lakeformation"); // These work
+    //    DISABLED_TESTS.add("dafny-dependencies/StandardLibrary"); // These work
   }
 
   @ParameterizedTest
@@ -98,10 +96,9 @@ class JavaTestModels extends TestModelTest {
     Assumptions.assumeFalse(DISABLED_TESTS.contains(relativeTestModelPath));
 
     Path testModelPath = getTestModelPath(relativeTestModelPath);
-//    make(testModelPath, "setup_prettier");
+    make(testModelPath, "setup_prettier");
     make(testModelPath, "polymorph_dafny");
     make(testModelPath, "polymorph_java");
-//    make(testModelPath, "transpile_java");
     make(testModelPath, "build_java");
     make(testModelPath, "test_java");
   }
