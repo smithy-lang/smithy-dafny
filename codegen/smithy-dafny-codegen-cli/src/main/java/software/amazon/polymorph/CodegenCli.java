a9d57f9215b8f60d57f307e2f6fc230589f823cf
Robin Salkeld <salkeldr@amazon.com>
2025-05-21 11:05:36 -0700

    Update codegen/smithy-dafny-codegen-cli/src/main/java/software/amazon/polymorph/CodegenCli.java
    Co-authored-by: Rishav karanjit <karanjitrishav4@gmail.com>

--- codegen/smithy-dafny-codegen-cli/src/main/java/software/amazon/polymorph/CodegenCli.java
+++ codegen/smithy-dafny-codegen-cli/src/main/java/software/amazon/polymorph/CodegenCli.java
@@ -496,7 +496,7 @@ public class CodegenCli {
       );
     new HelpFormatter()
       .printHelp(
-        "smithy-dafny-codegen-cli fs-dafny-at-least",
+        "smithy-dafny-codegen-cli if-dafny-at-least",
         getCliOptionsForIfDafnyAtLeast()
       );
   }
