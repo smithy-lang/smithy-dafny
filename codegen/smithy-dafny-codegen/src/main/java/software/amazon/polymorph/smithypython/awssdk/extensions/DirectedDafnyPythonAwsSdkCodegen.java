7bd58e2b2a17c2694e836793ffa30644193bceb8
Lucas McDonald <lucmcdon@amazon.com>
2025-05-22 12:31:00 -0700

    m

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithypython/awssdk/extensions/DirectedDafnyPythonAwsSdkCodegen.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithypython/awssdk/extensions/DirectedDafnyPythonAwsSdkCodegen.java
@@ -66,28 +66,14 @@ public class DirectedDafnyPythonAwsSdkCodegen extends DirectedPythonCodegen {
     GenerateServiceDirective<GenerationContext, PythonSettings> directive
   ) {}
 
-  /**
-   * Call `DirectedPythonCodegen.customizeAfterIntegrations`, then remove `models.py` and
-   * `errors.py`. The CodegenDirector will invoke this method after shape generation.
-   *
-   * @param directive Directive to perform.
-   */
-  @Override
-  public void customizeAfterIntegrations(
-    CustomizeDirective<GenerationContext, PythonSettings> directive
-  ) {
-    // DirectedPythonCodegen's customizeAfterIntegrations implementation SHOULD run first;
-    //   its implementation writes all files by flushing its writers;
-    //   this implementation removes some of those files.
-    super.customizeAfterIntegrations(directive);
-
+  private void removeTmpSymbolwriterDumpFile(CustomizeDirective<GenerationContext, PythonSettings> directive) {
     FileManifest fileManifest = directive.fileManifest();
     Path generationPath = Path.of(
       fileManifest.getBaseDir() +
-      "/" +
-      SmithyNameResolver.getServiceSmithygeneratedDirectoryNameForNamespace(
-        directive.context().settings().getService().getNamespace()
-      )
+        "/" +
+        SmithyNameResolver.getServiceSmithygeneratedDirectoryNameForNamespace(
+          directive.context().settings().getService().getNamespace()
+        )
     );
 
     /**
@@ -125,5 +111,31 @@ public class DirectedDafnyPythonAwsSdkCodegen extends DirectedPythonCodegen {
         e
       );
     }
+  }
+
+  /**
+   * Call `DirectedPythonCodegen.customizeAfterIntegrations`, then remove `models.py` and
+   * `errors.py`. The CodegenDirector will invoke this method after shape generation.
+   *
+   * @param directive Directive to perform.
+   */
+  @Override
+  public void customizeAfterIntegrations(
+    CustomizeDirective<GenerationContext, PythonSettings> directive
+  ) {
+    // DirectedPythonCodegen's customizeAfterIntegrations implementation SHOULD run first;
+    //   its implementation writes all files by flushing its writers;
+    //   this implementation removes some of those files.
+    try {
+      super.customizeAfterIntegrations(directive);
+    } catch (CodegenException e) {
+      if (e.getMessage().contains("python3 -m black . --exclude")) {
+        // If the tmp dump file contains invalid symbols, formatting will fail.
+        // Apparent workaround: Remove the tmp dump file, then re-run super's customize.
+        removeTmpSymbolwriterDumpFile(directive);
+        super.customizeAfterIntegrations(directive);
+      }
+    }
+    removeTmpSymbolwriterDumpFile(directive);
   }
 }
