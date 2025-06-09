97456490a31f37c28d2269b26bce4007ca0edf59
Rishav karanjit <karanjitrishav4@gmail.com>
2025-06-09 10:17:00 -0700

    chore(go): fix missing import  (#805)

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithygo/localservice/shapevisitor/SmithyToDafnyShapeVisitor.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithygo/localservice/shapevisitor/SmithyToDafnyShapeVisitor.java
@@ -166,13 +166,13 @@ public class SmithyToDafnyShapeVisitor extends ShapeVisitor.Default<String> {
     //Handle @reference{Service} shape
     if (resourceOrService.asServiceShape().isPresent()) {
       var clientConversion = dataSource.concat(".DafnyClient");
+      writer.addImportFromModule(
+        SmithyNameResolver.getGoModuleNameForSmithyNamespace(
+          resourceOrService.toShapeId().getNamespace()
+        ),
+        DafnyNameResolver.dafnyTypesNamespace(resourceOrService)
+      );
       if (resourceOrService.hasTrait(ServiceTrait.class)) {
-        writer.addImportFromModule(
-          SmithyNameResolver.getGoModuleNameForSmithyNamespace(
-            resourceOrService.toShapeId().getNamespace()
-          ),
-          DafnyNameResolver.dafnyTypesNamespace(resourceOrService)
-        );
         final var shim =
           "%swrapped.Shim".formatted(
               DafnyNameResolver.dafnyNamespace(
@@ -767,7 +767,12 @@ public class SmithyToDafnyShapeVisitor extends ShapeVisitor.Default<String> {
             .getProperty("Referred", Symbol.class)
             .get()
         );
-
+      writer.addImportFromModule(
+        SmithyNameResolver.getGoModuleNameForSmithyNamespace(
+          shape.toShapeId().getNamespace()
+        ),
+        DafnyNameResolver.dafnyTypesNamespace(shape)
+      );
       eachMemberInUnion.append(
         """
         case *%s.%s:
