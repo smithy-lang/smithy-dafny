076653efe2d10ba74cf0c1abdb9aa560be3c8b9e
Robin Salkeld <salkeldr@amazon.com>
2025-06-26 15:11:10 -0700

    Fix S3 Java

--- TestModels/aws-sdks/s3/runtimes/java/build.gradle.kts
+++ TestModels/aws-sdks/s3/runtimes/java/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("org.dafny:DafnyRuntime:${dafnyVersion}")
     implementation("software.amazon.smithy.dafny:conversion:0.1.1")
     implementation("software.amazon.cryptography:StandardLibrary:1.0-SNAPSHOT")
+    implementation("software.amazon.cryptography:StreamingSupport:1.0-SNAPSHOT")
     implementation(platform("software.amazon.awssdk:bom:2.31.30"))
     implementation("software.amazon.awssdk:s3")
 }
