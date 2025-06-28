54c962888c7d4e7ba1193be5bfa1269367f1245d
Robin Salkeld <salkeldr@amazon.com>
2025-06-28 10:56:45 -0700

    m

--- TestModels/dafny-dependencies/StandardLibrary/runtimes/java/build.gradle.kts
+++ TestModels/dafny-dependencies/StandardLibrary/runtimes/java/build.gradle.kts
@@ -63,7 +63,6 @@ repositories {
 dependencies {
     implementation("org.dafny:DafnyRuntime:${dafnyVersion}")
     implementation("software.amazon.smithy.dafny:conversion:0.1.1")
-    api("software.amazon.awssdk:sdk-core:2.28.28")
 }
 publishing {
     publications.create<MavenPublication>("mavenLocal") {
