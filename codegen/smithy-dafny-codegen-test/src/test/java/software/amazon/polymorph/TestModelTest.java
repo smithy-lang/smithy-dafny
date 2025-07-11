d535c113265673d6f1118ff2b7486141760e952c
Robin Salkeld <salkeldr@amazon.com>
2025-07-11 07:29:37 -0700

    m

--- codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/TestModelTest.java
+++ codegen/smithy-dafny-codegen-test/src/test/java/software/amazon/polymorph/TestModelTest.java
@@ -102,5 +102,10 @@ public abstract class TestModelTest {
     } else {
       Assumptions.assumeTrue(needsStreaming);
     }
+    
+    // StreamingSupport isn't directly testable
+    if (relativeTestModelPath.endsWith("StreamingSupport")) {
+      Assumptions.assumeTrue(false);
+    }
   }
 }
