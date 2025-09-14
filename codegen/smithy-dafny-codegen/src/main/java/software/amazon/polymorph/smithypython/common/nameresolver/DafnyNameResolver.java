bc9b83314dc1f7e995e7c57ae78bec76989eb86d
Siva Somayyajula <somayyas@amazon.com>
2025-09-14 18:02:05 -0400

    fix

--- codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithypython/common/nameresolver/DafnyNameResolver.java
+++ codegen/smithy-dafny-codegen/src/main/java/software/amazon/polymorph/smithypython/common/nameresolver/DafnyNameResolver.java
@@ -72,7 +72,27 @@ public class DafnyNameResolver {
       "try",
       "while",
       "with",
-      "yield"
+      "yield",
+      // Built-in functions (https://docs.python.org/3/library/functions.html)
+      "abs", "aiter", "all", "anext", "any", "ascii",
+      "bin", "bool", "breakpoint", "bytearray", "bytes"
+      "callable", "chr", "classmethod", "compile", "complex",
+      "delattr", "dict", "dir", "divmod",
+      "enumerate", "eval", "exec",
+      "filter", "float", "format", "frozenset",
+      "getattr", "globals",
+      "hasattr", "hash", "help", "hex",
+      "id", "input", "int", "isinstance", "issubclass", "iter",
+      "len", "list", "locals",
+      "map", "max", "memoryview", "min",
+      "next",
+      "object", "oct", "open", "ord",
+      "pow", "print", "property",
+      "range", "repr", "reversed", "round",
+      "set", "setattr", "slice", "sorted", "staticmethod", "str", "sum", "super",
+      "tuple", "type",
+      "vars",
+      "zip"
     )
   );
 
